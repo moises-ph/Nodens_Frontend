2660e582d1728c73586cf22cbbb989053d3e9b44
EthiemGuerrero <ethiemalex03@gmail.com>
2022-11-26 10:57:36 -0500

    app

---
+++ frontend/src/components/landing/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './landing.module.css'
+
+function Landing() {
+  return (
+    <>
+    <h1 className={style.x}>XD</h1>
+    </>
+  )
+}
+
+export default Landing
---
+++ frontend/src/components/nav/Nav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './nav.module.css'
+
+const Nav = () => {
+  return (
+    <></>
+  )
+}
+
+export default Nav
