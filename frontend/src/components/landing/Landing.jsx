b657d814babc12ba9085cba2f89ca03ee051eee7
EthiemGuerrero <ethiemalex03@gmail.com>
2022-11-26 12:27:35 -0500

    sty

--- frontend/src/components/landing/Landing.jsx
+++ frontend/src/components/landing/Landing.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import style from './landing.module.css'
+import React from "react";
+import style from "./landing.module.css";
 
 function Landing() {
   return (
     <>
-    <h1 className={style.x}>XD</h1>
+      
     </>
-  )
+  );
 }
 
-export default Landing
+export default Landing;
