a9f35f5da7569fdde1773aeabeddc7daa8cfaf71
EthiemGuerrero <ethiemalex03@gmail.com>
2022-11-26 15:45:38 -0500

    animacion nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Landing from './components/landing/Landing'
+
+const Nav = lazy(()=>import('./components/nav/Nav'))
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Nav />
         <Routes>
           <Route path='/' element={<Landing />}></Route>
         </Routes>
--- frontend/src/components/nav/Nav.jsx
+++ frontend/src/components/nav/Nav.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, {useState, useRef, useEffect} from 'react'
+import { Link } from 'react-router-dom'
 import style from './nav.module.css'
+import {IoEnter} from 'react-icons/io5'
+import {BsFillPersonPlusFill} from 'react-icons/bs'
 
 const Nav = () => {
+  const nav = useRef();
+  const [color, setColor] = useState(style.clear);
+
+
+  console.log(window.innerHeight);
+
+  useEffect(()=>{
+    const change = event => {
+      console.log(window.scrollY);
+      if(window.scrollY> window.innerHeight*0.85){
+        setColor(style.dark)
+      }
+      else {
+        setColor(style.clear)
+      }
+    }
+    window.addEventListener('scroll', change)
+    return ()=>{ window.removeEventListener('scroll', change)}
+  },[])
+
+  
+
   return (
-    <></>
+    <>
+      <nav className={`${style.nav} ${color}`}>
+        <h2><Link className={style.title} to='/'>NODENS</Link></h2>
+        <div className={style.links}>
+          <Link className={style.link} to='/login'>Sign In <BsFillPersonPlusFill /></Link>
+          <Link className={style.link} to='/registro'>Sign Up <IoEnter /></Link>
+        </div>
+      </nav>
+    </>
   )
 }
 
