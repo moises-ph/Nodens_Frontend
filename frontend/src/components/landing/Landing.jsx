92595971c48a1285a02de2a9b42380ec57d2277a
EthiemGuerrero <ethiemalex03@gmail.com>
2022-11-26 16:23:23 -0500

    animacion cards#

--- frontend/src/components/landing/Landing.jsx
+++ frontend/src/components/landing/Landing.jsx
@@ -1,9 +1,45 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import style from "./landing.module.css";
+import inte from '../../assets/inte.webp'
 
 function Landing() {
+  const [view, setView] = useState(style.out)
+  useEffect(()=>{
+    const sight = event => {
+      if(window.scrollY > window.innerHeight*0.70){
+        setView(style.in)
+      }
+    }
+    window.addEventListener('scroll', sight)
+    return ()=>{ window.removeEventListener('scroll', sight)}
+  },[])
+
   return (
     <>
+      <article className={style.article}>
+        <header className={style.head}>
+          <h1 className={style.title}>MUSIC</h1>
+        </header>
+      </article>
+
+      <main className={style.body}>
+        <section className={`${style.hacer} ${view}`}>
+          <img src={inte} className={style.inte} alt=""/>
+          <div className={style.card_des}>
+            <h4>Que Hacemos?</h4>
+            <p>Somos una aplicacion en busqueda de dar a conocer los artistas emergentes o sin experiencia laboral y exponerlos al mercado musical para que tengan mas oportunidades laborales</p>
+          </div>
+        </section>
+
+        <section className={`${style.hacer} ${view}`}>
+          <div className={style.card_pq}>
+            <h4>Porque?</h4>
+            <p>En el quindio y en Colombia en general hay muchos musicos prodigiosos y talentosos los cuales no han tenido la oportunidad de brillar o darse a conocer ya que no cuentan con los medios necesarios para lograrlo</p>
+          </div>
+          <img src={inte} className={style.inte} alt=""/>
+        </section>
+      </main>
+
       
     </>
   );
--- frontend/src/components/nav/Nav.jsx
+++ frontend/src/components/nav/Nav.jsx
@@ -1,19 +1,14 @@
-import React, {useState, useRef, useEffect} from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import style from './nav.module.css'
 import {IoEnter} from 'react-icons/io5'
 import {BsFillPersonPlusFill} from 'react-icons/bs'
 
 const Nav = () => {
-  const nav = useRef();
   const [color, setColor] = useState(style.clear);
-
-
-  console.log(window.innerHeight);
 
   useEffect(()=>{
     const change = event => {
-      console.log(window.scrollY);
       if(window.scrollY> window.innerHeight*0.85){
         setColor(style.dark)
       }
@@ -25,19 +20,17 @@ const Nav = () => {
     return ()=>{ window.removeEventListener('scroll', change)}
   },[])
 
-  
-
   return (
     <>
       <nav className={`${style.nav} ${color}`}>
         <h2><Link className={style.title} to='/'>NODENS</Link></h2>
         <div className={style.links}>
-          <Link className={style.link} to='/login'>Sign In <BsFillPersonPlusFill /></Link>
-          <Link className={style.link} to='/registro'>Sign Up <IoEnter /></Link>
+          <Link className={style.link} to='/login'>Sign In <BsFillPersonPlusFill style={{marginLeft: '5px'}}/></Link>
+          <Link className={style.link} to='/registro'>Sign Up <IoEnter style={{marginLeft: '5px'}}/></Link>
         </div>
       </nav>
     </>
   )
 }
 
-export default Nav
+export default Nav;
