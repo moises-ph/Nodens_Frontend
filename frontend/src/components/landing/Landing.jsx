712c4121a49030be407b440faebb9e8688e3a044
EthiemGuerrero <ethiemalex03@gmail.com>
2022-11-26 16:46:48 -0500

    landing

--- frontend/src/components/landing/Landing.jsx
+++ frontend/src/components/landing/Landing.jsx
@@ -1,8 +1,30 @@
 import React, {useEffect, useState} from "react";
 import style from "./landing.module.css";
 import inte from '../../assets/inte.webp'
+import {IoPersonCircleSharp} from 'react-icons/io5'
+import {BsFacebook} from 'react-icons/bs'
+import {AiFillGoogleCircle, AiOutlineInstagram} from 'react-icons/ai'
+import { Link } from "react-router-dom";
 
 function Landing() {
+  const testimonios = [{
+    title: 'lorem',
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis illo, impedit asperiores molestias rem consequuntur sunt obcaecati, a voluptatum facilis veniam optio dolores aspernatur maxime quaerat, debitis nulla dolore. Fuga.',
+    person: 'lorem ipsum'
+  },{
+    title: 'lorem',
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis illo, impedit asperiores molestias rem consequuntur sunt obcaecati, a voluptatum facilis veniam optio dolores aspernatur maxime quaerat, debitis nulla dolore. Fuga.',
+    person: 'lorem ipsum'
+  },{
+    title: 'lorem',
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis illo, impedit asperiores molestias rem consequuntur sunt obcaecati, a voluptatum facilis veniam optio dolores aspernatur maxime quaerat, debitis nulla dolore. Fuga.',
+    person: 'lorem ipsum'
+  },{
+    title: 'lorem',
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis illo, impedit asperiores molestias rem consequuntur sunt obcaecati, a voluptatum facilis veniam optio dolores aspernatur maxime quaerat, debitis nulla dolore. Fuga.',
+    person: 'lorem ipsum'
+  }]
+
   const [view, setView] = useState(style.out)
   useEffect(()=>{
     const sight = event => {
@@ -40,7 +62,29 @@ function Landing() {
         </section>
       </main>
 
-      
+      <article className={style.testimonios}>
+        {
+          testimonios.map((testimonio)=>{
+            return <div className={style.testi}>
+              <div className={style.icon}>
+                <IoPersonCircleSharp className={style.i_user}/>
+              </div>
+              <div className={style.info}>
+                <h4>{testimonio.title}</h4>
+                <div>
+                  <p className={style.card_descri}>{testimonio.description}</p>
+                  <ul className={style.social_n}>
+                    <Link to=''><BsFacebook className={style.min_icon}/></Link>
+                    <Link to=''><AiFillGoogleCircle className={style.min_icon}/></Link>
+                    <Link to=''><AiOutlineInstagram className={style.min_icon}/></Link>
+                  </ul>
+                </div>
+                <p className={style.p2}>{testimonio.person}</p>
+              </div>
+            </div>
+          })
+        }
+      </article>
     </>
   );
 }
