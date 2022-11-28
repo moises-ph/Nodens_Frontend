8ee5fed16d8f9a8c33dc1b4a15f0f3dc0da7947b
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-11-27 19:52:15 -0500

    xd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { lazy, Suspense } from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Footer from './components/footer/Footer'
 import Landing from './components/landing/Landing'
+import Error from './components/error/Error'
 
 const Nav = lazy(()=>import('./components/nav/Nav'))
 
@@ -10,9 +11,10 @@ function App() {
     <>
       <BrowserRouter>
         <Nav />
-        <Routes>
-          <Route path='/' element={<Landing />}></Route>
-        </Routes>
+          <Routes>
+            <Route path='/' element={<Landing />}></Route>
+            <Route path='*' element={<Error />}></Route>
+          </Routes>
         <Footer />
       </BrowserRouter>
     </>
