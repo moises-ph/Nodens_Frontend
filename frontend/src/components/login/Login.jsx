d2363431f554886feab31400e427bf8f247ec4bd
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-11-28 12:23:09 -0500

    login

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {BsFacebook} from 'react-icons/bs'
+import {AiFillGoogleCircle, AiOutlineInstagram} from 'react-icons/ai'
 import style from './login.module.css'
 
 function Login() {
   return (
     <>
-      <main class="main">
-        <div class="container-login">
-          <div class="login">
+      <main className={style.main}>
+        <div className={style.container_login}>
+          <div className={style.login}>
             <h2>LOGIN TO YOUR ACCOUNT</h2>
             <p>Login using social networks</p>
-            <div class="icons">
-              <Link to="/"><i class="fa-brands fa-facebook"></i></Link>
-              <Link to="/"><i class="fa-brands fa-google-plus"></i></Link>
-              <Link to="/"><i class="fa-brands fa-instagram"></i></Link>
+            <div className={style.icons}>
+              <Link to="/"><BsFacebook className={style.facebook}/></Link>
+              <Link to="/"><AiFillGoogleCircle className={style.google}/></Link>
+              <Link to="/"><AiOutlineInstagram className={style.instagram}/></Link>
             </div>
             <h3>OR</h3>
-            <input type="text" class="email-box" placeholder=" Email" />
-            <input type="text" class="password-box" placeholder=" Password" />
-            <Link to="/" class="forgot-box">Forgot your password?</Link>
-            <input type="button" class="login-button" value="Log in" />
+            <input type="text" className={style.email} placeholder=" Email" />
+            <input type="text" className={style.password} placeholder=" Password" />
+            <Link to="/" className={style.forgot}>Forgot your password?</Link>
+            <input type="button" className={style.login_button} value="Log in" />
           </div>
         </div>
       </main>
