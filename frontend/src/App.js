d01737113809b5e2236d08f5a135b48d7e597b4d
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-11-27 19:57:56 -0500

    loading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,27 @@
-import React, { lazy, Suspense } from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Footer from './components/footer/Footer'
-import Landing from './components/landing/Landing'
-import Error from './components/error/Error'
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Loading from './components/Loading/Loading'
 
-const Nav = lazy(()=>import('./components/nav/Nav'))
+const Nav = lazy(() => import("./components/nav/Nav"));
+const Landing = lazy(() => import("./components/landing/Landing"));
+const Footer = lazy(() => import("./components/footer/Footer"));
+const Error = lazy(() => import("./components/error/Error"));
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Nav />
+        <Suspense fallback={<Loading />}>
+          <Nav />
           <Routes>
-            <Route path='/' element={<Landing />}></Route>
-            <Route path='*' element={<Error />}></Route>
+            <Route path="/" element={<Landing />}></Route>
+            <Route path="*" element={<Error />}></Route>
           </Routes>
-        <Footer />
+          <Footer />
+        </Suspense>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
