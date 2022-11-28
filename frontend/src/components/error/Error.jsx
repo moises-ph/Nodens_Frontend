d0479935da971a2cc05f51194d5572bf8eeb2bd9
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-11-28 13:33:27 -0500

    error 404

--- frontend/src/components/error/Error.jsx
+++ frontend/src/components/error/Error.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import style from './error.module.css'
 
 function Error() {
   return (
-    <div>Error</div>
+    <>
+      <main className={style.body}>
+        <div className={style.container}>
+          <h1 className={style.error}>404</h1>
+          <p className={style.description}>Page not found...</p>
+        </div>
+      </main>
+    </>
   )
 }
 
