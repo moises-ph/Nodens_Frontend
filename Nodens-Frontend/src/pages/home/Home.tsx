ff44c073687d42ff4bc75365abb2e09df8b456c1
Sebastián García Correa <sebastian435622@gmail.com>
2023-02-23 15:02:31 -0500

    Xd

--- Nodens-Frontend/src/pages/home/Home.tsx
+++ Nodens-Frontend/src/pages/home/Home.tsx
@@ -1,7 +1,8 @@
 function Home() {
   return (
     <>
-      <h1 className="text-9xl">Home</h1>
+      <h1 className="text-9xl hover:text-red-500">Home</h1>
+      <h2 className="text-9xl text-white hover:text-black transition-all duration-1000">Ethiem se la come </h2>
     </>
   )
 }
