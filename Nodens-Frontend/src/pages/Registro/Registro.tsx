f27bf8e45f56fa3a07f65f8a0971286d8189fb34
Sebastián García <sebastian435622@gmail.com>
2023-02-23 21:03:38 -0500

    Tostada3

--- Nodens-Frontend/src/pages/Registro/Registro.tsx
+++ Nodens-Frontend/src/pages/Registro/Registro.tsx
@@ -1,18 +1,34 @@
 import axios from "axios";
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
 const Registro = () => {
+  const MySwal = withReactContent(Swal)
+
   const handleSubmit = (e: any) => {
     e.preventDefault();
     const form: FormData = new FormData(e.target);
     const object = Object.fromEntries(form)
+    console.log(object);
+    
   
     axios.post("https://localhost:44384/api/User/Register", object)
-      .then(res=>{console.log(res)})
+      .then(res=>{
+        console.log(res);
+        MySwal.fire({
+        position: 'top',
+        icon: 'success',
+        title: 'Usuario Registrado',
+        showConfirmButton: false,
+
+        timer: 1500
+      }
+      )})
       .catch(err=>{console.log(err)});
   };
 
-  return (
-    <main className='flex flex-col items-center pt-7 bg-slate-100 text-slate-100 gap-4 py-6 sm:py-4'>
+  return  (
+    <main className='flex flex-col items-center pt-7 bg-slate-100 text-slate-100 gap-4 py-6 sm:py-4'>    
       <h1 className="text-6xl text-zinc-900">Registro</h1>
       <form onSubmit={handleSubmit} className="w-5/6 sm:w-[400px] h-auto rounded-lg shadow-xl shadow-slate-900 flex flex-col justify-center items-start bg-zinc-900 pl-6 py-4 gap-4">
         <label htmlFor="Email" className="w-full flex flex-col gap-2">
@@ -21,19 +37,19 @@ const Registro = () => {
         </label>
         <label htmlFor="firstName" className="w-full flex flex-col gap-2">
           <h2>First Name</h2>
-          <input type="text" name="first_Name" id="" className="placeholder:text-slate-600 w-11/12 text-slate-100 bg-transparent border-solid border-b-2 border-b-slate-400 transition-colors duration-300 ease-linear focus:border-b-slate-100 focus:border-solid focus:border-b-2 outline-none leading-7" placeholder="First Name" required/>
+          <input type="text" minLength={3} name="first_Name" id="" className="placeholder:text-slate-600 w-11/12 text-slate-100 bg-transparent border-solid border-b-2 border-b-slate-400 transition-colors duration-300 ease-linear focus:border-b-slate-100 focus:border-solid focus:border-b-2 outline-none leading-7" placeholder="First Name" required/>
         </label>
         <label htmlFor="secondName" className="w-full flex flex-col gap-2">
           <h2>Second Name</h2>
-          <input type="text" name="second_Name" id="" className="placeholder:text-slate-600 w-11/12 text-slate-100 bg-transparent border-solid border-b-2 border-b-slate-400 transition-colors duration-300 ease-linear focus:border-b-slate-100 focus:border-solid focus:border-b-2 outline-none leading-7" placeholder="Second Name" required/>
+          <input type="text" minLength={3} name="second_Name" defaultValue={''} id="" className="placeholder:text-slate-600 w-11/12 text-slate-100 bg-transparent border-solid border-b-2 border-b-slate-400 transition-colors duration-300 ease-linear focus:border-b-slate-100 focus:border-solid focus:border-b-2 outline-none leading-7" placeholder="Second Name"/>
         </label>
         <label htmlFor="firstLastname" className="w-full flex flex-col gap-2">
           <h2>First Lastname</h2>
-          <input type="text" name="first_Lastname" id="" className="placeholder:text-slate-600 w-11/12 text-slate-100 bg-transparent border-solid border-b-2 border-b-slate-400 transition-colors duration-300 ease-linear focus:border-b-slate-100 focus:border-solid focus:border-b-2 outline-none leading-7" placeholder="First Lastname" required/>
+          <input type="text" minLength={3} name="first_Lastname" id="" className="placeholder:text-slate-600 w-11/12 text-slate-100 bg-transparent border-solid border-b-2 border-b-slate-400 transition-colors duration-300 ease-linear focus:border-b-slate-100 focus:border-solid focus:border-b-2 outline-none leading-7" placeholder="First Lastname" required/>
         </label>
         <label htmlFor="secondLastname" className="w-full flex flex-col gap-2">
           <h2>Second Lastname</h2>
-          <input type="text" name="second_Lastname" id="" className="placeholder:text-slate-600 w-11/12 text-slate-100 bg-transparent border-solid border-b-2 border-b-slate-400 transition-colors duration-300 ease-linear focus:border-b-slate-100 focus:border-solid focus:border-b-2 outline-none leading-7" placeholder="Second Lastname" required/>
+          <input type="text" minLength={3} name="second_Lastname" defaultValue={''} id="" className="placeholder:text-slate-600 w-11/12 text-slate-100 bg-transparent border-solid border-b-2 border-b-slate-400 transition-colors duration-300 ease-linear focus:border-b-slate-100 focus:border-solid focus:border-b-2 outline-none leading-7" placeholder="Second Lastname" />
         </label>
         <label htmlFor="rol" className="w-full flex flex-col gap-2">
           <h2>Rol</h2>
@@ -53,7 +69,7 @@ const Registro = () => {
         </label>
         <label htmlFor="password" className="w-full flex flex-col gap-2">
           <h2>Password</h2>
-          <input type="password" name="password" id="" className="placeholder:text-slate-600 w-11/12 text-slate-100 bg-transparent border-solid border-b-2 border-b-slate-400 transition-colors duration-300 ease-linear focus:border-b-slate-100 focus:border-solid focus:border-b-2 outline-none leading-7" placeholder="Password" required/>
+          <input type="password" name="password" minLength={8} id="" className="placeholder:text-slate-600 w-11/12 text-slate-100 bg-transparent border-solid border-b-2 border-b-slate-400 transition-colors duration-300 ease-linear focus:border-b-slate-100 focus:border-solid focus:border-b-2 outline-none leading-7" placeholder="Password" required/>
         </label>
         <input type="submit" value="Submit" className="place-self-center py-2 px-4 bg-transparent text-red-600 font-semibold border border-red-600 rounded hover:bg-red-600 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0" />
       </form>
