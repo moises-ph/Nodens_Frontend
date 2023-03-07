b1aca3af89e14306bed9474c385152c03eb93ec4
EthiemGuerrero <ethiemalex03@gmail.com>
2023-03-06 21:08:42 -0500

    reacomodarse al backend

--- Nodens-Frontend/src/pages/Login/Login.tsx
+++ Nodens-Frontend/src/pages/Login/Login.tsx
@@ -1,7 +1,29 @@
+import axios from "axios";
+
 const Login = () => {
+  const handleSubmit = (e:any) => {
+    e.preventDefault();
+    const form: FormData = new FormData(e.target);
+    const data = Object.fromEntries(form);
+
+    console.log(data);
+    
+    axios.post('https://localhost:44384/', data)
+      .then(res => console.log(res))
+      .catch(err=> console.log(err))
+  }
   return (
     <>
-      <div>Login</div>
+      <main>
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="">Email</label>
+          <input type="email" name="email" id="" placeholder="Email"/>
+          <label htmlFor="password">Contraseña</label>
+          <input type="password" name="password" id="" placeholder="Contraseña"/>
+          <input type="submit" className="cursor-pointer" value="Iniciar Sesion" />
+        </form>
+      </main>
     </>
   )
 }
