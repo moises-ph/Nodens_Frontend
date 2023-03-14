99d800bae2d554c25ee7cadf779e14b943c2a524
Sebastián García Correa <sebastian435622@gmail.com>
2023-03-14 16:02:02 -0500

    terminar nav

--- Nodens-Frontend/src/Routers/InitialRouter.tsx
+++ Nodens-Frontend/src/Routers/InitialRouter.tsx
@@ -1,10 +1,31 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { Home, Login, Registro, Error } from '../pages'
+import {Nav} from '../components'
+import { links } from '../components/Nav/Nav'
+
+const routes: links[] = [
+  {
+    icon: '',
+    route: '/',
+    text: 'Home'
+  },
+  {
+    icon: '',
+    route: '/registro',
+    text: 'Registro'
+  },
+  {
+    icon: '',
+    route: '/login',
+    text: 'Login'
+  }
+]
 
 export const InitialRouter = () => {
   return (
     <>
     	<Router>
+        <Nav links={routes}/>
         <Routes>
           <Route path='/' element={<Home />}></Route>
           <Route path='/registro' element={<Registro />}></Route>
