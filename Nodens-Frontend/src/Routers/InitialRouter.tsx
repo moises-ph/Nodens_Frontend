f91f390e2ff3b46f37ae5f8188c4b2363d355451
EthiemGuerrero <ethiemalex03@gmail.com>
2023-03-14 21:03:16 -0500

    terminar

--- Nodens-Frontend/src/Routers/InitialRouter.tsx
+++ Nodens-Frontend/src/Routers/InitialRouter.tsx
@@ -1,31 +1,20 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import { Home, Login, Registro, Error } from '../pages'
 import {Nav} from '../components'
-import { links } from '../components/Nav/Nav'
-
-const routes: links[] = [
-  {
-    icon: '',
-    route: '/',
-    text: 'Home'
-  },
-  {
-    icon: '',
-    route: '/registro',
-    text: 'Registro'
-  },
-  {
-    icon: '',
-    route: '/login',
-    text: 'Login'
-  }
-]
+import {HiMenu} from 'react-icons/hi'
+import { useState } from 'react'
 
 export const InitialRouter = () => {
+  const [showNav, setShowNav] = useState<boolean>(false)
   return (
     <>
     	<Router>
-        <Nav links={routes}/>
+        <Nav inView={showNav} setShowNav={setShowNav}/>
+        <header className="fixed w-full flex justify-between items-center text-slate-100 py-4 px-4 bg-slate-900 shadow-lg z-50">
+          <Link to='/' className='cursor-pointer'><h1 className="text-2xl">Nodens</h1></Link>
+          <button onClick={()=>setShowNav(true)}><HiMenu /></button>
+        </header>
+        <Nav inView={showNav} setShowNav={setShowNav}/>
         <Routes>
           <Route path='/' element={<Home />}></Route>
           <Route path='/registro' element={<Registro />}></Route>
--- Nodens-Frontend/src/components/Nav/Nav.tsx
+++ Nodens-Frontend/src/components/Nav/Nav.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import UseAnimation from 'react-useanimations/'
+import React, { Dispatch, SetStateAction } from 'react'
+import { Link } from 'react-router-dom'
+import {FaHome, FaShoppingBag} from 'react-icons/fa'
+import {AiOutlineUserAdd} from 'react-icons/ai'
 
-const nav = () => {
+interface NavBar {
+    inView: boolean;
+    setShowNav:  Dispatch<SetStateAction<boolean>>
+  }
+
+const Nav:React.FC<NavBar> = ({inView, setShowNav}) => {
+  if(!inView) return null
   return (
-    <>
-    </>
+    <div className="z-[100] w-full h-screen fixed flex flex-col justify-start items-end backdrop-blur-sm inset-0 pr-4 pt-4" onClick={()=>setShowNav(false)}>
+        <nav className="bg-slate-700 h-3/5 w-5/6 flex flex-col items-start gap-5 pt-4 pl-4 rounded-xl sm:w-3/5 md:w-2/4 lg:w-1/4">
+          <div className="h-[10%] w-full flex items-center">
+            <Link className="h-[10%] w-11/12 flex items-center gap-2 rounded-lg text-slate-100 text-xl py-4 pl-2 transition-colors duration-300 ease-linear hover:bg-slate-400" to="/"><FaHome /> Home</Link>
+            <button className="text-2xl text-slate-100 place-self-end mb-4" onClick={()=>setShowNav(false)}>X</button>
+          </div>
+          <Link className="h-[10%] w-11/12 flex items-center gap-2 rounded-lg text-slate-100 text-xl py-4 pl-2 transition-colors duration-300 ease-linear hover:bg-slate-400" to="/products"><AiOutlineUserAdd /> Registro</Link>
+        </nav>
+      </div>
   )
 }
 
-export default nav
+export default Nav
