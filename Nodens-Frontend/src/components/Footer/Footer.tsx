5705915ca4359a3e7abce19ff0a50bb7a6aea4c7
Sebastian <sebastian435622@gmail.com>
2023-03-17 18:25:12 -0500

    shi

--- Nodens-Frontend/src/components/Footer/Footer.tsx
+++ Nodens-Frontend/src/components/Footer/Footer.tsx
@@ -6,11 +6,11 @@ import {FaInstagram} from "react-icons/fa"
 const Footer = () => {
   return (
     <>
-      <footer className="bg-slate-900 flex justify-center items-center flex-col gap-4"> 
+      <footer className="absolute w-full bg-slate-900 flex justify-center items-center flex-col gap-4"> 
         <div className="text-2xl text-slate-50">Nodens</div>
         <p className="text-center text-slate-50">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
         <div className="flex justify-center items-center flex-row">
-        <Link rel="stylesheet" className="text-5xl text-slate-50 transition-colors duration-75 hover:" to=""><AiFillFacebook /></Link>
+        <Link rel="stylesheet" className="text-5xl text-slate-50 transition-colors duration-75 " to=""><AiFillFacebook className="h-14 w-14 hover:text-[#1877F2]"/></Link>
         <Link rel="stylesheet" className="text-5xl text-slate-50" to=""><FaInstagram /></Link>
         <Link rel="stylesheet" className="text-5xl text-slate-50" to=""><AiFillGithub /></Link>
         </div>
