d6b13af293448e5c5d0b7e4b4d9cc1a25ea237c7
Sebastian <sebastian435622@gmail.com>
2023-03-17 21:12:21 -0500

    shi

--- Nodens-Frontend/src/components/Footer/Footer.tsx
+++ Nodens-Frontend/src/components/Footer/Footer.tsx
@@ -1,18 +1,18 @@
 import { Link } from "react-router-dom";
-import {FiGithub, FiInstagram} from "react-icons/fi"
-import { RiFacebookCircleFill} from "react-icons/ri"
-import {TbBrandGithub} from 'react-icons/tb'
+import { FiInstagram } from "react-icons/fi"
+import { RiFacebookCircleFill } from "react-icons/ri"
+import { TbBrandGithub } from 'react-icons/tb'
 
 const Footer = () => {
   return (
     <>
       <footer className="absolute h-40 w-full bg-slate-900 flex justify-center items-center flex-col gap-4"> 
-        <div className="text-2xl text-slate-50">Nodens</div>
+        <div className="text-2xl text-slate-50 font-serif drop-shadow-2xl shadow-white ">Nodens</div>
         <p className="text-center text-slate-50">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
         <div className="flex justify-center items-center flex-row gap-4">
-        <Link rel="stylesheet" className="text-5xl text-slate-50 transition-colors duration-75 " to=""><RiFacebookCircleFill className="text-5xl rounded-full transition-all duration-200 ease-in hover:text-[#1877F2] hover:bg-slate-50 hover:text-6xl"/></Link>
-        <Link rel="stylesheet" className="text-5xl text-slate-50" to=""><FiInstagram  className="text-5xl rounded-2xl transition-all duration-200 ease-in hover:bg-gradient-to-br from-[#803C96] to-[#FE0900] hover:bg-slate-50 hover:text-6xl"/></Link>
-        <Link rel="stylesheet" className="text-5xl text-slate-50" to=""><TbBrandGithub className="text-5xl rounded-full transition-all duration-200 ease-in hover:text-black hover:bg-slate-50 hover:text-6xl"/></Link>
+        <Link rel="stylesheet" className="text-5xl text-slate-50 transition-colors duration-75 " to=""><RiFacebookCircleFill className="text-4xl rounded-full transition-all duration-200 ease-in hover:text-[#1877F2] hover:bg-slate-50 hover:text-5xl"/></Link>
+        <Link rel="stylesheet" className="text-5xl text-slate-50" to=""><FiInstagram  className="text-4xl rounded-2xl transition-all duration-200 ease-in hover:bg-gradient-to-br from-[#803C96] to-[#FE0900] hover:bg-slate-50 hover:text-5xl"/></Link>
+        <Link rel="stylesheet" className="text-5xl text-slate-50" to=""><TbBrandGithub className="text-4xl rounded-full transition-all duration-200 ease-in hover:text-black hover:bg-slate-50 hover:text-5xl"/></Link>
         </div>
       </footer>
     </>
