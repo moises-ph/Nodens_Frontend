af99ef29c5f60eeeeba30e9faf70d9af92302195
Sebastián <sebastian435622@gmail.com>
2023-03-23 21:10:26 -0500

    perfiles de músicos

--- Nodens-Frontend/src/pages/Profile/Profile.tsx
+++ Nodens-Frontend/src/pages/Profile/Profile.tsx
@@ -1,302 +1,301 @@
-import React from 'react'
-
+import React from "react";
+import { FaUserCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
 export type InstrumentoT = {
-  Nombre: String,
-  Nivel : String
-  
-}
-  export type postPro = {
-    IdAuth: String,
-    Fecha_Nacimiento: Date,
-    Instrumentos: InstrumentoT[],
-  GenerosMusicales : String,
-  Pais : String,
-  Ciudad: String,
-  Experiencia: String,
+  Nombre: String;
+  Nivel: String;
+};
+export type postPro = {
+  IdAuth: String;
+  Fecha_Nacimiento: Date;
+  Instrumentos: InstrumentoT[];
+  GenerosMusicales: String;
+  Pais: String;
+  Ciudad: String;
+  Experiencia: String;
   Educacion: [
     {
-      Nombre: String,
-      Institucion: String,
-      Fecha_Inicio: Date,
-      Fecha_Fin: Date
-    } 
-],
-  Url_Foto_Perfil: String,
-  Url_Video_Presentacion: String,
+      Nombre: String;
+      Institucion: String;
+      Fecha_Inicio: Date;
+      Fecha_Fin: Date;
+    }
+  ];
+  Url_Foto_Perfil: String;
+  Url_Video_Presentacion: String;
   Redes_Sociales: [
     {
-      Nombre: String,
-      Url: String
+      Nombre: String;
+      Url: String;
     }
-  ]
-}
+  ];
+};
 
-const profile :  postPro[] =[
+const profile: postPro[] = [
   {
     IdAuth: "1",
     Fecha_Nacimiento: new Date(),
-    Instrumentos:[
-       {
-          Nombre: "Guitarra",
-          Nivel: "Avanzado"
-       },
-       {
+    Instrumentos: [
+      {
+        Nombre: "Guitarra",
+        Nivel: "Avanzado",
+      },
+      {
         Nombre: "piano",
-        Nivel: "Avanzado"
-     }
-     ],
-     GenerosMusicales: "De todo",
-     Pais: "Colombia",
-     Ciudad: "Armenia",
-     Experiencia: "10 Años",
-     Educacion: [
-        {
-          Nombre: "Sebastián",
-          Institucion: "Fundanza",
-          Fecha_Inicio: new Date(),
-          Fecha_Fin: new Date()
-        }
-      ],
-      Url_Foto_Perfil: "ichbineinmann.24",
-      Url_Video_Presentacion: "wirbieneinemanner.77",
-      Redes_Sociales: [
-        {
-          Nombre: "ichwilleinehundin",
-          Url: "pwihrefiugf"
-          
-        }
-      ]
-   },
-   {
+        Nivel: "Avanzado",
+      },
+    ],
+    GenerosMusicales: "De todo",
+    Pais: "Colombia",
+    Ciudad: "Armenia",
+    Experiencia: "10 Años",
+    Educacion: [
+      {
+        Nombre: "Sebastián",
+        Institucion: "Fundanza",
+        Fecha_Inicio: new Date(),
+        Fecha_Fin: new Date(),
+      },
+    ],
+    Url_Foto_Perfil: "ichbineinmann.24",
+    Url_Video_Presentacion: "wirbieneinemanner.77",
+    Redes_Sociales: [
+      {
+        Nombre: "ichwilleinehundin",
+        Url: "pwihrefiugf",
+      },
+    ],
+  },
+  {
     IdAuth: "1",
     Fecha_Nacimiento: new Date(),
-    Instrumentos:[
-       {
-          Nombre: "Guitarra",
-          Nivel: "Avanzado"
-       }
-     ],
-     GenerosMusicales: "De todo",
-     Pais: "Colombia",
-     Ciudad: "Armenia",
-     Experiencia: "10 Años",
-     Educacion: [
-        {
-          Nombre: "Sebastián",
-          Institucion: "Fundanza",
-          Fecha_Inicio: new Date(),
-          Fecha_Fin: new Date()
-        }
-      ],
-      Url_Foto_Perfil: "ichbineinmann.24",
-      Url_Video_Presentacion: "wirbieneinemanner.77",
-      Redes_Sociales: [
-        {
-          Nombre: "ichwilleinehundin",
-          Url: "pwihrefiugf"
-          
-        }
-      ]
-   },
-   {
+    Instrumentos: [
+      {
+        Nombre: "Guitarra",
+        Nivel: "Avanzado",
+      },
+    ],
+    GenerosMusicales: "De todo",
+    Pais: "Colombia",
+    Ciudad: "Armenia",
+    Experiencia: "10 Años",
+    Educacion: [
+      {
+        Nombre: "Sebastián",
+        Institucion: "Fundanza",
+        Fecha_Inicio: new Date(),
+        Fecha_Fin: new Date(),
+      },
+    ],
+    Url_Foto_Perfil: "ichbineinmann.24",
+    Url_Video_Presentacion: "wirbieneinemanner.77",
+    Redes_Sociales: [
+      {
+        Nombre: "ichwilleinehundin",
+        Url: "pwihrefiugf",
+      },
+    ],
+  },
+  {
     IdAuth: "1",
     Fecha_Nacimiento: new Date(),
-    Instrumentos:[
-       {
-          Nombre: "Guitarra",
-          Nivel: "Avanzado"
-       },
-       {
+    Instrumentos: [
+      {
+        Nombre: "Guitarra",
+        Nivel: "Avanzado",
+      },
+      {
         Nombre: "piano",
-        Nivel: "Avanzado"
-     }
-     ],
-     GenerosMusicales: "De todo",
-     Pais: "Colombia",
-     Ciudad: "Armenia",
-     Experiencia: "10 Años",
-     Educacion: [
-        {
-          Nombre: "Sebastián",
-          Institucion: "Fundanza",
-          Fecha_Inicio: new Date(),
-          Fecha_Fin: new Date()
-        }
-      ],
-      Url_Foto_Perfil: "ichbineinmann.24",
-      Url_Video_Presentacion: "wirbieneinemanner.77",
-      Redes_Sociales: [
-        {
-          Nombre: "ichwilleinehundin",
-          Url: "pwihrefiugf"
-          
-        }
-      ]
-   },
-   {
+        Nivel: "Avanzado",
+      },
+    ],
+    GenerosMusicales: "De todo",
+    Pais: "Colombia",
+    Ciudad: "Armenia",
+    Experiencia: "10 Años",
+    Educacion: [
+      {
+        Nombre: "Sebastián",
+        Institucion: "Fundanza",
+        Fecha_Inicio: new Date(),
+        Fecha_Fin: new Date(),
+      },
+    ],
+    Url_Foto_Perfil: "ichbineinmann.24",
+    Url_Video_Presentacion: "wirbieneinemanner.77",
+    Redes_Sociales: [
+      {
+        Nombre: "ichwilleinehundin",
+        Url: "pwihrefiugf",
+      },
+    ],
+  },
+  {
     IdAuth: "1",
     Fecha_Nacimiento: new Date(),
-    Instrumentos:[
-       {
-          Nombre: "Guitarra",
-          Nivel: "Avanzado"
-       }
-     ],
-     GenerosMusicales: "De todo",
-     Pais: "Colombia",
-     Ciudad: "Armenia",
-     Experiencia: "10 Años",
-     Educacion: [
-        {
-          Nombre: "Sebastián",
-          Institucion: "Fundanza",
-          Fecha_Inicio: new Date(),
-          Fecha_Fin: new Date()
-        }
-      ],
-      Url_Foto_Perfil: "ichbineinmann.24",
-      Url_Video_Presentacion: "wirbieneinemanner.77",
-      Redes_Sociales: [
-        {
-          Nombre: "ichwilleinehundin",
-          Url: "pwihrefiugf"
-          
-        }
-      ]
-   }, {
+    Instrumentos: [
+      {
+        Nombre: "Guitarra",
+        Nivel: "Avanzado",
+      },
+    ],
+    GenerosMusicales: "De todo",
+    Pais: "Colombia",
+    Ciudad: "Armenia",
+    Experiencia: "10 Años",
+    Educacion: [
+      {
+        Nombre: "Sebastián",
+        Institucion: "Fundanza",
+        Fecha_Inicio: new Date(),
+        Fecha_Fin: new Date(),
+      },
+    ],
+    Url_Foto_Perfil: "ichbineinmann.24",
+    Url_Video_Presentacion: "wirbieneinemanner.77",
+    Redes_Sociales: [
+      {
+        Nombre: "ichwilleinehundin",
+        Url: "pwihrefiugf",
+      },
+    ],
+  },
+  {
     IdAuth: "1",
     Fecha_Nacimiento: new Date(),
-    Instrumentos:[
-       {
-          Nombre: "Guitarra",
-          Nivel: "Avanzado"
-       },
-       {
+    Instrumentos: [
+      {
+        Nombre: "Guitarra",
+        Nivel: "Avanzado",
+      },
+      {
         Nombre: "piano",
-        Nivel: "Avanzado"
-     }
-     ],
-     GenerosMusicales: "De todo",
-     Pais: "Colombia",
-     Ciudad: "Armenia",
-     Experiencia: "10 Años",
-     Educacion: [
-        {
-          Nombre: "Sebastián",
-          Institucion: "Fundanza",
-          Fecha_Inicio: new Date(),
-          Fecha_Fin: new Date()
-        }
-      ],
-      Url_Foto_Perfil: "ichbineinmann.24",
-      Url_Video_Presentacion: "wirbieneinemanner.77",
-      Redes_Sociales: [
-        {
-          Nombre: "ichwilleinehundin",
-          Url: "pwihrefiugf"
-          
-        }
-      ]
-   }, {
+        Nivel: "Avanzado",
+      },
+    ],
+    GenerosMusicales: "De todo",
+    Pais: "Colombia",
+    Ciudad: "Armenia",
+    Experiencia: "10 Años",
+    Educacion: [
+      {
+        Nombre: "Sebastián",
+        Institucion: "Fundanza",
+        Fecha_Inicio: new Date(),
+        Fecha_Fin: new Date(),
+      },
+    ],
+    Url_Foto_Perfil: "ichbineinmann.24",
+    Url_Video_Presentacion: "wirbieneinemanner.77",
+    Redes_Sociales: [
+      {
+        Nombre: "ichwilleinehundin",
+        Url: "pwihrefiugf",
+      },
+    ],
+  },
+  {
     IdAuth: "1",
     Fecha_Nacimiento: new Date(),
-    Instrumentos:[
-       {
-          Nombre: "Guitarra",
-          Nivel: "Avanzado"
-       }
-     ],
-     GenerosMusicales: "De todo",
-     Pais: "Colombia",
-     Ciudad: "Armenia",
-     Experiencia: "10 Años",
-     Educacion: [
-        {
-          Nombre: "Sebastián",
-          Institucion: "Fundanza",
-          Fecha_Inicio: new Date(),
-          Fecha_Fin: new Date()
-        }
-      ],
-      Url_Foto_Perfil: "ichbineinmann.24",
-      Url_Video_Presentacion: "wirbieneinemanner.77",
-      Redes_Sociales: [
-        {
-          Nombre: "ichwilleinehundin",
-          Url: "pwihrefiugf"
-          
-        }
-      ]
-   }, {
+    Instrumentos: [
+      {
+        Nombre: "Guitarra",
+        Nivel: "Avanzado",
+      },
+    ],
+    GenerosMusicales: "De todo",
+    Pais: "Colombia",
+    Ciudad: "Armenia",
+    Experiencia: "10 Años",
+    Educacion: [
+      {
+        Nombre: "Sebastián",
+        Institucion: "Fundanza",
+        Fecha_Inicio: new Date(),
+        Fecha_Fin: new Date(),
+      },
+    ],
+    Url_Foto_Perfil: "ichbineinmann.24",
+    Url_Video_Presentacion: "wirbieneinemanner.77",
+    Redes_Sociales: [
+      {
+        Nombre: "ichwilleinehundin",
+        Url: "pwihrefiugf",
+      },
+    ],
+  },
+  {
     IdAuth: "1",
     Fecha_Nacimiento: new Date(),
-    Instrumentos:[
-       {
-          Nombre: "Guitarra",
-          Nivel: "Avanzado"
-       },
-       {
+    Instrumentos: [
+      {
+        Nombre: "Guitarra",
+        Nivel: "Avanzado",
+      },
+      {
         Nombre: "piano",
-        Nivel: "Avanzado"
-     }
-     ],
-     GenerosMusicales: "De todo",
-     Pais: "Colombia",
-     Ciudad: "Armenia",
-     Experiencia: "10 Años",
-     Educacion: [
-        {
-          Nombre: "Sebastián",
-          Institucion: "Fundanza",
-          Fecha_Inicio: new Date(),
-          Fecha_Fin: new Date()
-        }
-      ],
-      Url_Foto_Perfil: "ichbineinmann.24",
-      Url_Video_Presentacion: "wirbieneinemanner.77",
-      Redes_Sociales: [
-        {
-          Nombre: "ichwilleinehundin",
-          Url: "pwihrefiugf"
-          
-        }
-      ]
-   }
-]
-
+        Nivel: "Avanzado",
+      },
+    ],
+    GenerosMusicales: "De todo",
+    Pais: "Colombia",
+    Ciudad: "Armenia",
+    Experiencia: "10 Años",
+    Educacion: [
+      {
+        Nombre: "Sebastián",
+        Institucion: "Fundanza",
+        Fecha_Inicio: new Date(),
+        Fecha_Fin: new Date(),
+      },
+    ],
+    Url_Foto_Perfil: "ichbineinmann.24",
+    Url_Video_Presentacion: "wirbieneinemanner.77",
+    Redes_Sociales: [
+      {
+        Nombre: "ichwilleinehundin",
+        Url: "pwihrefiugf",
+      },
+    ],
+  },
+];
 
 const Profile = () => {
   return (
     <>
-    <div>Profile</div>
-
-    {profile.map((profile1 , index)=>{
-    return(
-      <div key={index}>
-      <p>{profile1.Educacion.map((name, index2)=>{
-        return(
-          <span key={index2}>
-            <p>{name.Nombre}</p>
-          </span>
-        )
-      })}</p>
-      <p>{profile1.Experiencia}</p>
-      <p className='flex'>{profile1.Instrumentos.map((instrumentos, inst)=>{
-          return(
-            <span key={inst}>
-              <p>{instrumentos.Nombre},</p>
-            </span>
-        ) 
-      }
-    )
-  }</p>
-      </div>
-     )
-    }
-  )
-}
+      <main>
+        <p className="border-b-2 pt-2 pl-4 pb-4 shadow-xl text-3xl">Perfiles de <span className="border-b-2 text-red-500 ">Músicos</span></p>
+        {profile.map((profile1, index) => {
+          return (
+            <div key={index} className="border-b-2 pb-2 flex pl-">
+              <Link to=""><FaUserCircle className="text-[3rem] ml-2 mt-6"/></Link>
+               <div className="pl-4">
+               <p>
+                {profile1.Educacion.map((name, index2) => {
+                  return (
+                    <span className="" key={index2}>
+                      <p className="text-2xl ">{name.Nombre}</p>
+                    </span>
+                  );
+                })}
+              </p>
+              <p className="text-lg">{profile1.Experiencia}</p>
+              <p className="flex">
+                {profile1.Instrumentos.map((instrumentos, inst) => {
+                  return (
+                    <span className="flex align-center justify-center" key={inst}>
+                      <p>{instrumentos.Nombre},</p>
+                    </span>
+                  );
+                })}
+              </p>
+               </div>
+            </div>
+          );
+        })}
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
