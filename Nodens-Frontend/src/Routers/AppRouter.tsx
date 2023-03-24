a8f7f3a3a70510d06c47d93406aab8e87d425468
Ethiem <ethiemalex03@gmail.com>
2023-03-24 18:24:44 -0500

    tipos

--- Nodens-Frontend/src/Routers/AppRouter.tsx
+++ Nodens-Frontend/src/Routers/AppRouter.tsx
@@ -1,19 +1,20 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import {App, Error, Offers, Posts, Profiles} from '../pages'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { App, Error, Offers, Posts, Profiles } from "../pages";
 
 export const AppRouter = () => {
-	return (
-		<>
-			<Router>
-				<main>
-					<Routes>
-						<Route path='/' element={<App />}></Route>
-						<Route path='/posts' element={<Posts />}></Route>
-						<Route path='/offers' element={<Offers />}></Route>
-						<Route path='/profile' element={<Profiles />}></Route>						<Route path='*' element={<Error />}></Route>
-					</Routes>
-				</main>
-			</Router>
-		</>
-	)
-}
+  return (
+    <>
+      <Router>
+        <main>
+          <Routes>
+            <Route path="/" element={<App />}></Route>
+            <Route path="/posts" element={<Posts />}></Route>
+            <Route path="/offers" element={<Offers />}></Route>
+            <Route path="/profile" element={<Profiles />}></Route>{" "}
+            <Route path="*" element={<Error />}></Route>
+          </Routes>
+        </main>
+      </Router>
+    </>
+  );
+};
---
+++ Nodens-Frontend/src/types/profile.ts
@@ -0,0 +1,30 @@
+export type InstrumentoT = {
+    Nombre: String;
+    Nivel: String;
+  };
+  
+  export type ProfileT = {
+    IdAuth: String;
+    Fecha_Nacimiento: Date;
+    Instrumentos: InstrumentoT[];
+    GenerosMusicales: String;
+    Pais: String;
+    Ciudad: String;
+    Experiencia: String;
+    Educacion: [
+      {
+        Nombre: String;
+        Institucion: String;
+        Fecha_Inicio: Date;
+        Fecha_Fin: Date;
+      }
+    ];
+    Url_Foto_Perfil: String;
+    Url_Video_Presentacion: String;
+    Redes_Sociales: [
+      {
+        Nombre: String;
+        Url: String;
+      }
+    ];
+  };
