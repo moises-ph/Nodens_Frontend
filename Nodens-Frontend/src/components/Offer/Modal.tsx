e7e04d8da98e960d697f7bc4d593a2535aea53a9
Sebastián <sebastian435622@gmail.com>
2023-03-24 21:18:44 -0500

    Terminar ofertas

--- Nodens-Frontend/src/components/Offer/Modal.tsx
+++ Nodens-Frontend/src/components/Offer/Modal.tsx
@@ -2,6 +2,7 @@ import {motion} from 'framer-motion'
 import { Link } from 'react-router-dom'
 import { OffersT } from '../../types'
 import Offer from './Offer'
+import { BiHeartCircle } from "react-icons/bi"
 
 const variants = {
 	open: {
@@ -21,9 +22,14 @@ const Modal = ({open, oferta}:{open:boolean, oferta: OffersT | undefined}) => {
 		exit={{translateY: '100vh'}}
 	>
 		<Offer oferta={oferta}/>
-			<div className='flex justify-center items-center pt-56'>
-				<Link to="" className='fixed h-10 w-28 bg-slate-300 rounded-2xl shadow-xl shadow-slate-400 flex items-center justify-center'>Postularme</Link>
+			<div className='flex justify-center items-center pt-52 w-full pr-16'>
+				<Link to="" className='fixed h-10 w-[10rem] bg-slate-300 rounded-2xl shadow-xl shadow-slate-400 flex items-center justify-center bg-blue-300 transition-all duration-200 '>Postularme</Link>
+				<div >
+				<Link to="" className=''><BiHeartCircle  className='text-4xl ml-[16rem] hover:text-red-500 hover:bg-red-100 w-10  transition-all duration-200 rounded-full'/></Link>
+				</div>
 			</div>
+		
+
 			
 
 	</motion.div>
