b5848dbd5ad3b83f35557f2afaa8370770ea9c26
Sebastián García Correa <sebastian435622@gmail.com>
2023-03-27 11:43:14 -0500

    Postulare link

--- Nodens-Frontend/src/components/Offer/Modal.tsx
+++ Nodens-Frontend/src/components/Offer/Modal.tsx
@@ -18,12 +18,12 @@ const Modal = ({open, oferta}:{open:boolean, oferta: OffersT | undefined}) => {
 	<motion.div
 		animate={open ? 'open' : 'closed'}
 		variants={variants}
-		className='h-[90vh] overflow-y-scroll fixed w-full bg-slate-100 rounded-lg shadow-2xl shadow-slate-300 shadow-inner rounded-t-xl'
+		className='h-[90vh] overflow-y-scroll fixed w-full bg-slate-100 rounded-lg  shadow-slate-300 shadow-inner rounded-t-xl'
 		exit={{translateY: '100vh'}}
 	>
 		<Offer oferta={oferta}/>
-			<div className='flex justify-center items-center pt-64 w-full pr-16'>
-				<Link to="" className='fixed h-10 w-[10rem] bg-slate-300 rounded-2xl shadow-xl shadow-slate-400 flex items-center justify-center bg-blue-300 transition-all duration-200 '>Postularme</Link>
+			<div className='flex justify-center items-center pt-28 w-full pr-16'>
+				<Link to="" className='fixed h-10 w-[10rem] rounded-2xl shadow-xl shadow-slate-400 flex items-center justify-center bg-blue-300 transition-all duration-200 '>Postularme</Link>
 				<div >
 				<Link to="" className=''><BiHeartCircle  className='text-4xl ml-[16rem] hover:text-red-500 hover:bg-red-100 w-10  transition-all duration-200 rounded-full'/></Link>
 				</div>
