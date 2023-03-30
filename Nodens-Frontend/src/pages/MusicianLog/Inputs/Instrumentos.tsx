00624dd1435c8ef7fa9a79f86e65b0ca09d8dfec
Ethiem <ethiemalex03@gmail.com>
2023-03-29 20:42:41 -0500

    hacer logica de los instrumentos

--- Nodens-Frontend/src/pages/MusicianLog/Inputs/Instrumentos.tsx
+++ Nodens-Frontend/src/pages/MusicianLog/Inputs/Instrumentos.tsx
@@ -11,17 +11,15 @@ const Instrumentos = ({ handler, goBack }: { handler: (key: string, value: any) 
   const experiencia = useRef(null)
 
   const addInstrument = () => {
-    console.log('hola');
-    
     setInstrumentos([...instrumentos, 
       {nombre: instrumento.current.value, experiencia: experiencia.current.value}]
     )
   }
   return (
-    <div className='bg-slate-200 h-3/4 border-solid border-2 border-slate-400 rounded-lg w-10/12 flex flex-col justify-center gap-8 px-2'>
-      <label htmlFor="instrumentos" className='text-2xl h-2/4 flex flex-col gap-[20%]'>
+    <div className='bg-slate-200 h-full border-solid border-2 border-slate-400 rounded-lg w-10/12 flex flex-col justify-center gap-10 px-2'>
+      <div className='text-2xl h-2/4 flex flex-col gap-[20%]'>
         <p>Instrumentos:</p>
-        <div>
+        <div className='grid grid-cols-3 h-14 w-11/12'>
           {
             instrumentos.map((instr, i)=>{
               return <div key={i}>
@@ -30,16 +28,20 @@ const Instrumentos = ({ handler, goBack }: { handler: (key: string, value: any) 
             })
           }
         </div>
-        <input type="text" name="instrumentos" ref={instrumento} className='bg-transparent border-solid border-2 border-slate-400 rounded-md text-slate-700 font-medium'/>
-        <select name="" id="" ref={experiencia} onInput={e=> addInstrument()}>
-          <option value="" disabled >Experiencia</option>
-          <option value="Menos de 1 año">Menos de 1 año</option>
-          <option value="Mas de 1 año">Mas de 1 año</option>
-          <option value="Mas de 2 años">Mas de 2 años</option>
-          <option value="Mas de 5 años">Mas de 5 años</option>
-        </select>
-      </label>
-      <div>
+        <label htmlFor="" className="w-11/12">Instrumento:
+          <input type="text" name="instrumentos" ref={instrumento} className='w-full bg-transparent border-solid border-2 border-slate-400 rounded-md text-slate-700 font-medium'/>
+        </label>
+        <label htmlFor="">Experiencia: 
+          <select name="" id="" ref={experiencia} onInput={e=> addInstrument()}>
+            <option value="" disabled >Experiencia</option>
+            <option value="Menos de 1 año">Menos de 1 año</option>
+            <option value="Mas de 1 año">Mas de 1 año</option>
+            <option value="Mas de 2 años">Mas de 2 años</option>
+            <option value="Mas de 5 años">Mas de 5 años</option>
+          </select>
+        </label>
+      </div>
+      <div className="flex gap-2">
         <button onClick={() => goBack()}>Atras</button>
         <button onClick={() =>handler('instrumentos', instrumento.current.value)}>Guardar</button>
       </div>
