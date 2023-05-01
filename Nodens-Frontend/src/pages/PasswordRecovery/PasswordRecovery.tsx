633349390002d8c4c21d72a0eaecb26f38822199
moisesph <mois.mp8@gmail.com>
2023-04-30 22:56:34 -0500

    Terminar recovery

--- Nodens-Frontend/src/pages/PasswordRecovery/PasswordRecovery.tsx
+++ Nodens-Frontend/src/pages/PasswordRecovery/PasswordRecovery.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { FormEvent, useEffect, useState } from 'react'
 import { Logo } from '../../components'
 import { useSearchParams } from 'react-router-dom'
 import Swal from 'sweetalert2';
@@ -20,48 +20,89 @@ function PasswordRecovery() {
     const verifyRecovery = (gdusr : string, mn : string) => {
         console.log(gdusr);
         console.log(mn);
-        
+        Swal.fire({
+            title : "Validando información...",
+            allowEscapeKey : false,
+            showCancelButton : false,
+            showConfirmButton : false
+        });
         fetch(`http://20.253.63.70/api/auth/recovery/request?gdusr=${gdusr}&mn=${mn}`, {
             method : "POST"
-        }).then(res => {
-            console.log(res);
+        }).then(async res => {
             if(res.status != 200){
-                throw new Error();
+                let data = await res.json();
+                throw new Error(data.msg);
             }
-            res.json();
+            else{
+                return res.json();
+            }
         }).then((data : any) =>{
-            console.log(data);
+            Swal.close();
+            setToken(data.token);
         }).catch(err => {
-            console.log(err);
-            
+            setTimeout(() => location.replace("/") , 4000);
             Swal.fire({
                 title : "Hubo un error :(",
-                text : err,
+                text : err + " Será redirigido en 4 segundos",
                 icon : 'error',
-                allowEscapeKey : false
+                allowEscapeKey : false,
+                showCancelButton : false,
+                showConfirmButton : false
             })
         })
     }
     
+    const changePass = (e : any) => {
+        e.preventDefault();
+        const form: FormData = new FormData(e.target);
+        const data : any = Object.fromEntries(form);
+        console.log(data);
+        if(data.pass1 != data.pass2) Swal.fire({
+            title : "Cuidado!",
+            text : "Las contraseñas deben coincidir",
+            icon : 'info',
+            showConfirmButton : true
+        })
+        else{
+            let body  = {
+                Password : data.pass1
+            }
+            fetch("http://20.253.63.70/api/auth/recovery/reset",{
+                body : JSON.stringify(body),
+                method : "POST",
+                headers : {
+                    Authorization : token
+                }
+            })
+        }
+    }
     
     useEffect(() => {
         if(gdusr != null && mn != null){
             setIsQuery(true);
-            verifyRecovery(gdusr, mn);
+            // verifyRecovery(gdusr, mn);
         }
     },[])
+
+    useEffect(()=>{
+        console.log(token);
+    }, [token]);
     
     
     return (
         isQuery ? <>
             <main className='bg-gradient-to-br flex flex-col from-[#B701F7] to-[#E79A77] min-h-screen min-w-full justify-center items-center gap-2'>
-                <Logo dimensions='h-28 w-28' />
-                <h1 className='text-2xl font-semibold text-slate-100'>Restablece tu contraseña</h1>
-                <form className='flex flex-col items-center gap-3 p-4'>
-                    <input />
-                    <input />
-                    <button type='submit'>Cambiar Contraseña</button>
-                </form>
+                <div className='flex flex-col items-center justify-center bg-slate-100 rounded-lg w-1/4 min-h-[70vh] gap-5 p-3 shadow-2xl'>
+                    <Logo dimensions='h-28 w-28' />
+                    <h1 className='text-2xl font-semibold pt-2'>Restablece tu contraseña</h1>
+                    <form onSubmit={changePass} className='flex flex-col gap-3 p-4 w-full'>
+                        <label htmlFor="pass1">Introduce tu nueva contraseña</label>
+                        <input name="pass1" id='pass1' className='bg-slate-200 transition ease-in-out focus:scale-105 p-1 border-solid border-slate-800 border' type='password' />
+                        <label htmlFor="pass2" className='pt-4' >Introduce tu nueva contraseña</label>
+                        <input name="pass2" id='pass2' className='bg-slate-200 transition ease-in-out focus:scale-105 p-1 border-solid border-slate-800 border' type='password' />
+                        <button type='submit' className='rounded bg-[#B701F7] w-1/2 h-9 text-slate-200 font-semibold transition ease-in-out self-center hover:bg-[#bc4ae5] hover:scale-105'>Cambiar Contraseña</button>
+                    </form>
+                </div>
             </main>
         </>
         :
