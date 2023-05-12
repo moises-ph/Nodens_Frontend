66da3cff8db2fb7630ec0e5a0cc906fb855fe2d0
Sebastián García Correa <sebastian435622@gmail.com>
2023-05-12 09:55:32 -0500

    Ya hice el modal, falta funcionalidad

--- Nodens-Frontend/src/pages/CreatePost/CreatePost.tsx
+++ Nodens-Frontend/src/pages/CreatePost/CreatePost.tsx
@@ -8,23 +8,24 @@ import Button from "react-bootstrap/esm/Button";
 const CreatePost = () => {
   const [show, setShow] = useState(false);
 
-  const handleClose = () => (false);
-  const handleShow = () => (true);
+  const handleClose = () => setShow (false);
+  const handleShow = () => setShow (true);
 
   return (
     <>
       <main className="bg-slate-900 min-h-screen min-w-full">
         <div className="pt-2 pl-20 ">
           <div className="bg-opacity-10 bg-slate-100 w-44 h-10 rounded-md flex items-center justify-center ">
-            <Button variant="primary" onClick={handleShow} className="text-slate-50 pl-2 flex justify-center items-center gap-2 "><Link to=""><BsFileEarmarkPost /></Link>  Crear publicación </Button>
-            <Modal show={show} onHide={handleClose}>
-              <Modal.Header closeButton>
-                <Modal.Title>Modal</Modal.Title>
+            <Button variant="primary" onClick={handleShow} className="text-slate-50 pl-2 flex justify-center items-center gap-2 "><p><BsFileEarmarkPost /></p> 
+             Crear publicación </Button>
+            <Modal show={show} onHide={handleClose} className="min-h-[40rem]">
+              <Modal.Header closeButton className="">
+                <Modal.Title></Modal.Title>
               </Modal.Header>
               <Modal.Body>EEEEEo</Modal.Body>
-              <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>Close</Button>
-                <Button variant="primary" onClick={handleClose}>Save</Button>
+              <Modal.Footer className="flex justify-end items-end pt-[32rem] gap-2">
+                <Button variant="secondary" onClick={handleClose} className="pr-4 bg-red-200 w-14 text-center">Close</Button>
+                <Button variant="primary" onClick={handleClose} className="pr-2 bg-green-200 w-14 text-center">Save</Button>
               </Modal.Footer>
             </Modal>
           </div>
