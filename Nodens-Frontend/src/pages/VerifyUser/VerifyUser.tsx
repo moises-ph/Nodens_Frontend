d01ba2a94b1df17420f95765ef17ed436a2573c2
moisesph <mois.mp8@gmail.com>
2023-05-17 16:50:56 -0500

    Verificación de Usuario terminada

--- Nodens-Frontend/src/pages/VerifyUser/VerifyUser.tsx
+++ Nodens-Frontend/src/pages/VerifyUser/VerifyUser.tsx
@@ -1,28 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
+import axios from 'axios';
+import { FcApproval, FcCancel } from 'react-icons/fc';
 
 function VerifyUser() {
 
     const [isValid, setValid] = useState(false);
     const [isLoad, setLoad] = useState(false);
+    const [success, setSuccess] = useState(false);
     const [searchParams, setSearchParams] = useSearchParams();
-    const [message, setMessage] = useState("Verificado")
+    const [message, setMessage] = useState("Verificando...")
     const guid : string | null = searchParams.get("guid");
-    const email : string | null = searchParams.get("emailHash");
+    const email : string | null = searchParams.get("em");
 
     const isLoading = () => isLoad;
 
-    const fetchVerifyUser = (guidp : string, emailp : string) => {
+    const fetchVerifyUser = () => {
         setLoad(true);
-        console.log(guidp);
-        console.log(emailp);
-        setTimeout(()=> setLoad(false), 1000)
+        axios.put(`https://nodensapim.azure-api.net/auth/api/auth/verify?em=${email}&guid=${guid}`)
+            .then(data => {
+                setLoad(false);
+                setSuccess(true);
+                setMessage(data.data.message);
+            })
+            .catch(err => {
+                setLoad(false);
+                console.log(err.response.data.message);
+                setMessage(err.response.data.message);
+            });
     }
 
     useEffect(()=>{
         if(guid && email){
             setValid(true);
-            fetchVerifyUser(guid, email);
+            fetchVerifyUser();
         };
     }, []);
 
@@ -30,8 +41,16 @@ function VerifyUser() {
         isValid ? 
         <>
             <main className='bg-gradient-to-br flex flex-col from-[#B701F7] to-[#E79A77] min-h-screen min-w-full justify-center items-center gap-2'>
-                {isLoading() && <><div className='w-[56px] h-[56px] rounded-[50%] [border-left-style:solid] border-[11.2px] border-double border-[#474bff] animate-spin'></div></>}
-                {!isLoading() && <><h1 className='text-2xl font-semibold text-slate-100'>{message}</h1></>}
+                {isLoad ? 
+                    <div className='w-[60px] h-[60px] rounded-[50%] [border-left-style:solid] border-[11.2px] border-double border-[#474bff] animate-spin'></div> 
+                    : 
+                    success ?
+                    <FcApproval className='w-[60px] h-[60px] motion-safe:animate-bounce'></FcApproval>
+                    : 
+                    <FcCancel className='w-[60px] h-[60px] animate-pulse'></FcCancel>
+                }
+                <h1 className='text-2xl font-semibold text-slate-100'>{message}</h1>
+                {!isLoading() && <Link to="/" className='h-10 text-center text-2xl font-bold rounded-md shadow-md hover:shadow-lg hover:scale-110 transition text-slate-300 p-0 w-48 bg-purple-600 mt-5'>Volver a Inicio</Link>}
             </main>
         </>
         :
