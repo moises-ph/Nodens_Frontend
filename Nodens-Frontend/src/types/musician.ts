fc32b5bb5f3fb1952828864ffe02c725652e5e6a
EthiemGuerrero <ethiemalex03@gmail.com>
2023-05-24 14:24:07 -0500

    some chnages

--- Nodens-Frontend/src/types/musician.ts
+++ Nodens-Frontend/src/types/musician.ts
@@ -1,19 +1,24 @@
 import { InstrumentoT } from "./profile";
 
 export type MusicianT = {
+  IdAuth?: string;
   fecha_nacimiento: Date;
+  Name: string;
+  Lastname: string;
   instrumentos: InstrumentoT[];
-  generosMusicales: String[];
-  pais: String;
-  ciudad: String;
-  experiencia: String;
+  generosMusicales: string[];
+  genero: string;
+  telefono: string;
+  pais: string;
+  ciudad: string;
+  experiencia: string;
   educacion: {
-    nombre: String;
-    institucion: String;
+    nombre: string;
+    institucion: string;
     fecha_Inicio: Date;
     fecha_Fin: Date;
   }[];
-  url_foto_perfil: String;
-  url_video_presentacion: String;
-  redes_rociales: {nombre: String;url: String}[];
+  url_foto_perfil: string;
+  url_video_presentacion: string;
+  redes_sociales: {nombre: string;url: string}[];
 };
