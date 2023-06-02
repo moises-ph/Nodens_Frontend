71d0fc60a8be89d93e1ea31849ba52206c1d5d36
EthiemGuerrero <ethiemalex03@gmail.com>
2023-06-02 16:43:35 -0500

    odio los graficos su ptm

--- Nodens-Frontend/src/pages/AppOrganizer/views/CellphView.tsx
+++ Nodens-Frontend/src/pages/AppOrganizer/views/CellphView.tsx
@@ -1,6 +1,7 @@
-import { Chart } from 'react-google-charts'
 import { OffersT, OrganizerT, ProfileT } from '../../../types'
+import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts'
 import DefaultUserImage from '../../../assets/DefaultUser.webp'
+import { useState } from 'react'
 
 export type chartT = {
   profile: OrganizerT,
@@ -9,31 +10,54 @@ export type chartT = {
 }
 
 const CellphView = ({profile, profiles, offers}: chartT) => {
-  const data = [
-    ["Info", "info for the organizer dashboard"],
-    ["vacants", 10],
-    ["offers", 20]
-  ]
-  const options = {
-    title: "info",
-    pieHole: 0.3,
-    is3D: false
+  const [data, setData] = useState([
+    {name: "Group A", value: offers.filter(of=> of.isAvailable).length},
+    {name: "Group B", value: offers.filter(of=> !of.isAvailable).length}
+  ]);
+ 
+  const RADIAN = Math.PI/180;
+  const renderCustomeLabel = ({cx, cy, midAngle, innerRadius, outerRadius, percent, index}) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius + Math.cos(-midAngle * RADIAN);
+    const y = cy + radius + Math.cos(-midAngle * RADIAN);
+    return (
+      <text x={x} y={y} fill="white" textAnchor={x> cx ? "start" : "end"} dominantBaseline="central">
+       {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    )
   }
   return (
     <section className="w-full h-fit">
-      <div className="h-[30vh]">
-        <div className="flex items-center gap-2 h-2/4">
-          <img src={profile.url_foto_perfil || DefaultUserImage} alt="profile pic" className="h-4/5 w-auto"/>
-          <h3>{profile.Name} {profile.Lastname}</h3>
+      <div className="h-[25vh] flex items-center gap-2">
+        <img src={profile.url_foto_perfil || DefaultUserImage} alt="profile pic" className="h-4/5 w-auto"/>
+        <div className="">
+          <h3 className="text-xl">{profile.Name} {profile.Lastname}</h3>
+          <p className="text-sm text-slate-500">Organizador</p>
+          <p>{profile.telefono}</p>
         </div>
       </div>
-      <Chart 
-        chartType='PieChart'  
-        width="100%"
-        height="300px"
-        data={data}
-        options={options}
-      />
+      {
+        offers.length === 0 
+        ? <h2 className="text-2xl text-center">No tienes ninguna oferta publicada</h2>
+        : <PieChart width={300}height={300}>
+              <Pie
+                data={data}
+                cx={200}
+                cy={200}
+                labelLine={false}
+                label={renderCustomeLabel}
+                outerRadius={80}
+                fill="#8884d8"
+                dataKey="value"
+              >
+                {
+                  data.map((entry, index)=> (
+                    <Cell key={`cell-${index}`} fill='#ee4466' />
+                  ))
+                }
+              </Pie>
+            </PieChart>
+      }
     </section>
   )
 }
