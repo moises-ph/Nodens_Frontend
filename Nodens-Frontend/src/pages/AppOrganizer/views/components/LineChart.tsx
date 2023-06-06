90a13612d0b0b43b01fa79616025e8facb3ac363
EthiemGuerrero <ethiemalex03@gmail.com>
2023-06-05 19:49:06 -0500

    shi

--- Nodens-Frontend/src/pages/AppOrganizer/views/components/LineChart.tsx
+++ Nodens-Frontend/src/pages/AppOrganizer/views/components/LineChart.tsx
@@ -1,10 +1,38 @@
 import { Chart , CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
+import { OffersT } from '../../../../types';
 Chart.register( CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend );
 
-const LineChart = () => {
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const
+    },
+    title: {
+      display: true,
+      text: 'Info de tus ofertas',
+    }
+  }
+}
+
+const labels = ['Numero de ofertas', 'Ofertas con aplicantes', 'Promedio aplicantes por oferta'];
+
+const LineChart = ({offers}: {offers: OffersT[]}) => {
+  const data = {
+    labels,
+    datasets: [{
+      label: 'Ofertas',
+      data: [
+        offers.length,
+        offers.filter(off=> off.Applicants.length > 0).length,
+        offers.reduce((accumulator, currentValue) => accumulator + currentValue.Applicants.length, 0) / offers.length
+      ]
+
+    }]
+  }
   return (
-    <div>LineChart</div>
+    <Bar options={options} data={data} />
   )
 }
 
