41e80719b27752a3a0bf466989568d076519331e
EthiemGuerrero <ethiemalex03@gmail.com>
2023-06-07 18:15:25 -0500

    organizer profile for musicians

--- Nodens-Frontend/src/pages/SingleOrganizer/SingleOrganizer.tsx
+++ Nodens-Frontend/src/pages/SingleOrganizer/SingleOrganizer.tsx
@@ -40,6 +40,10 @@ const SingleOrganizer = () => {
       .catch(err=> console.log(err))
   }
 
+  const hasCompany = () : boolean => organizer!.nombre_empresa.length > 0 && organizer!.descripcion_empresa.length > 0
+
+  const hasCompanyLogo = () => organizer!.url_logo != null
+
   useEffect(()=> {
     getOrganizer();
     getUserEmail();
@@ -47,7 +51,51 @@ const SingleOrganizer = () => {
   if(!organizer) return <Loading />
 
   return (
-    <div>SingleOrganizer</div>
+    <article className="flex justify-center">
+      <section className="slide-top min-h-screen flex w-full md:w-1/2 flex-col items-center gap-4 transition"> 
+        <div className=" flex flex-col gap-1 items-center px-4">
+          <div className="min-w-[70%] h-[300px] shadow-lg absolute bg-orange-500 bg-opacity-100 md:w-1/3 z-[1] rounded-b-full" />
+          <div className="flex flex-col gap-1 items-center my-4 p-4 z-10 ">
+            <img src={organizer.url_foto_perfil || DefaultUserImg} loading="lazy" alt="Organizer Profile" className="rounded-full h-40 w-40 object-cover mb-1" />
+            <div className="flex flex-col items-center text-slate-100">
+              <h1 className="text-center text-3xl organizerNameFont"><span className="font-semibold">{organizer.Name}</span> <span className="organizerNameFont font-thin">{organizer.Lastname}</span></h1>
+              <h3 className="font-light">{email}</h3>
+            </div>
+          </div>
+        </div>
+        <div className="bg-white rounded-2xl drop-shadow-xl grid grid-cols-2 grid-rows-2 w-3/5 gap-3 grid-flow-col-dense md:w-2/5 p-3">
+          <div className="w-full md:flex md:flex-col md:items-center organizerNameFont mb-2"><span className="font-semibold text-gray-900">Genero:</span> <span className="font-extralight text-gray-700">{organizer.genero}</span></div>
+          <div className="w-full md:flex md:flex-col md:items-center organizerNameFont mb-2"><span className="font-semibold text-gray-900">Telefono:</span> <span className="font-extralight text-gray-700">{organizer.telefono}</span></div>
+          <div className="w-full md:flex md:flex-col md:items-center organizerNameFont"><span className="font-semibold text-gray-900">Edad:</span> <span className="font-extralight text-gray-700">{new Date().getFullYear() - new Date(organizer.fecha_nacimiento).getFullYear()}</span></div>
+          <div className="w-full md:flex md:flex-col md:items-center organizerNameFont"><span className="font-semibold text-gray-900">Ciudad:</span> <span className="font-extralight text-gray-700">{organizer.ciudad}</span></div>
+        </div>
+        { hasCompany() &&
+          <div className="bg-white rounded-2xl drop-shadow-xl p-4 flex flex-col w-4/5 md:w-2/3">
+            <span className="font-semibold text-gray-900 text-base">Empresa:</span> 
+            <h2 className="organizerNameFont mb-2 text-2xl">{organizer.nombre_empresa}</h2>
+            <p className="text-base">{organizer.descripcion_empresa}</p>
+            { hasCompanyLogo() && <img src={organizer.url_logo} alt={`(${organizer.nombre_empresa} logo)`} className="text-sm italic"></img> }
+          </div>
+        }
+        <div className="bg-white rounded-2xl drop-shadow-xl p-3 flex flex-col w-4/5 md:w-2/3 gap-2">
+          <h2 className="font-semibold text-gray-900">Redes Sociales:</h2>
+          <div className="flex justify-evenly flex-wrap place-items-center gap-3">
+          {organizer.redes_sociales.length > 0 ? 
+          organizer.redes_sociales.map((socialmedia, index) => {
+            return(
+              <div key={index} className="flex flex-col items-center hover:scale-110 transition w-fit">
+                <a href={socialmedia.url} className="text-lg hover:underline w-fit">{SocialMedias[socialmedia.nombre.toLowerCase()]}</a>
+                <span className="text-xs">{socialmedia.nombre}</span>
+              </div>
+            )
+          })
+          :
+          <div><span className="font-extralight text-gray-700">Tus Redes sociales van aquí</span></div>
+          }
+          </div>
+        </div>
+      </section>
+    </article>
   )
 }
 
