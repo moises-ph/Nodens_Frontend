9549255d2e07564e425698d5142f10d322a5622d
Camilo Forero <forero5515@gmail.com>
2023-06-17 16:16:53 -0500

    Seguir con offers: Moises

--- Nodens-Frontend/src/components/Offers/Offers.tsx
+++ Nodens-Frontend/src/components/Offers/Offers.tsx
@@ -1,11 +1,13 @@
 import { AnimatePresence } from "framer-motion"
 import { useEffect, useRef, useState } from "react"
-import { BsSearch } from "react-icons/bs"
+import { BsChevronCompactDown, BsFillBookmarkFill, BsSearch } from "react-icons/bs"
 import { Filters, Loading, Modal, SingleOffer } from "../../components"
 import { OffersT } from "../../types"
 import { renewToken } from "../../services"
 import { clientHttp } from "../../services/client"
 import { BsFilterRight, BsArrowRepeat } from "react-icons/bs"
+import { Link } from "react-router-dom"
+import {VscSettings} from "react-icons/vsc";
 
 const Offers = () => {
 	const [modal, setOpen] = useState(false);
@@ -13,6 +15,8 @@ const Offers = () => {
 	const [offers,setOffers] = useState<OffersT[] | null>(null);
 	const [isOpen, setIsOpen] = useState<boolean>(false);
 	const searchInput = useRef<HTMLInputElement>(null);
+
+	const [filter, setFilter] = useState<boolean>(false);
 
 	const getOffersByTag = async () => {
 		await renewToken();
@@ -66,19 +70,70 @@ const Offers = () => {
 	if(!offers) return <Loading />
 	return (
 		<>
-			<section className="h-full overflow-y-hidden ">
-				<div className="flex flex-col md:top-[25.666667%] pt-3 md:absolute w-full md:w-2/5 overflow-y-scroll gap-2 p-2">
+			<section className="h-full overflow-y-hidden pt-10 flex justify-center gap-5 bg-zinc-100">
+				<div className="bg-white w-[17rem] max-w-[17rem] p-4 rounded-md h-fit">
+					<Link to="/applicants-offers" className="h-full flex items-center gap-1">
+						<BsFillBookmarkFill className="text-zinc-500" />
+						<span>Mis Ofertas Guardadas</span>
+					</Link>
+					<div className="bg-white py-4">
+						<button className="w-fit flex items-center gap-1" onClick={()=> setFilter(!filter)}>
+							<VscSettings className="h-5 w-5" />
+							<h2 className="text-xl">Filtrar ofertas</h2>
+							<BsChevronCompactDown className={`transition-all duration-300 filter ${filter ? 'rotate-180' : ''}`} />
+						</button>
+						<div className={filter ? '' : 'hidden'}>
+							<div className="flex flex-col gap-4 mb-6 ">
+							<label className="flex flex-col gap-2 font-thin ">Por Pago
+								<input type='number' placeholder="Pago minimo" className="rounded-lg px-2 border-[1px] border-slate-500"/>
+								<input type='number' placeholder="Pago Maximo" className="rounded-lg px-2 border-[1px] border-slate-500"/>
+							</label>
+							<label className="flex flex-col gap-2 font-thin ">Por Requerimiento:
+								<select className="rounded-lg px-2 border-[1px] border-slate-500">
+								<optgroup>
+									<option value="">Instrumento</option>
+									<option value="Guitarra">Guitarra</option>
+									<option value="Bajo">Bajo</option>
+									<option value="Bateria">Bateria</option>
+									<option value="Piano">Piano</option>
+									<option value="Congas">Congas</option>
+									<option value="Bongo">Bongo</option>
+									<option value="Saxofon">Saxofon</option>
+									<option value="Timbal">Timbal</option>
+									<option value="Voz">Voz</option>
+									<option value="Trompeta">Trompeta</option>
+								</optgroup>
+								</select>
+							</label>
+							<label className="flex flex-col gap-2 font-thin ">Por Fecha de creacion: 
+								<input type='date' placeholder="fecha de creacion" className="rounded-lg px-2 border-[1px] border-slate-500"/>
+							</label>
+							<label className="flex flex-col gap-2 font-thin ">Por Fecha del Evento:
+								<input type='date' placeholder="fecha de evento" className="rounded-lg px-2 border-[1px] border-slate-500"/>
+							</label>
+							</div>
+							<div className="flex justify-evenly gap-4 ">
+							<button className="rounded-md w-1/2 p-2 bg-red-500 text-slate-100">Cancelar</button>
+							<button className="rounded-md w-1/2 p-2 bg-green-500 text-slate-100">Filtrar</button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="flex flex-col pt-3 w-[38rem] overflow-y-scroll gap-2 p-4 rounded-xl bg-white">
+					<div className="w-full h-fit py-3">
+						<h2 className="text-xl font-semibold">Recomendaciones</h2>
+						<p className="font-thin">En función de tu perfil y tu historial de búsqueda</p>
+					</div>
 					{
 						offers.map((offer, i)=> {
 							return <SingleOffer showModal={showModal} redirect={null} offer={offer} key={i} Key={i.toString()} isHomePage={false} /> 
 						})
 					}
 				</div>
-				<AnimatePresence>
+				{/* <AnimatePresence>
 					{modal && <Modal open={modal} closeModal={closeModal} oferta={oferta}/>}
-				</AnimatePresence>
+				</AnimatePresence> */}
 			</section>
-			<Filters isOpen={isOpen} setOffers={setOffers} getOffers={getInitialOffers} offers={offers} setIsOpen={setIsOpen}/>
 		</>
 	)
 }
--- Nodens-Frontend/src/components/SingleOffer/SingleOffer.tsx
+++ Nodens-Frontend/src/components/SingleOffer/SingleOffer.tsx
@@ -9,14 +9,11 @@ function SingleOffer(props : any) {
     ${creation_date.getMonth()+1 < 10 ? '0'+Number(creation_date.getMonth()+1): creation_date.getMonth()+1} -
     ${creation_date.getFullYear()}`
 
-
-  const classDiv = `w-full min-h-full flex flex-col p-4 border-2 border-solid border-slate-300 gap-1 rounded-lg transition-colors hover:bg-slate-100 hover:cursor-pointer ${props.isHomePage ? "backdrop-blur-md shadow-md" : null}`
-
   return (
-    <div onClick={()=> props.isHomePage ? props.redirect() : props.showModal(props.offer)} key={parseInt(props.Key)} className={classDiv}>
+    <div onClick={()=> props.isHomePage ? props.redirect() : props.showModal(props.offer)} key={parseInt(props.Key)} className={`group w-full min-h-fit flex flex-col pr-0 pt-4 border-b border-b-solid border-slate-300 gap-1 transition-colors rounded-b-none hover:cursor-pointer ${props.isHomePage ? "backdrop-blur-md shadow-md" : null}`}>
       <div className="flex gap-2">
         <BsPersonSquare className="h-8 w-8 text-sky-500" />
-        <h3 className="text-xl font-semibold">{props.offer.Title}</h3>
+        <h3 className="text-xl font-semibold group-hover:underline text-blue-700">{props.offer.Title}</h3>
       </div>
         <p><span className="text-slate-500">Ubicacion: </span>{props.offer.Event_Ubication.City}, {props.offer.Event_Ubication.Town}</p>
         <p>Pago: {props.offer.Payment}</p>
