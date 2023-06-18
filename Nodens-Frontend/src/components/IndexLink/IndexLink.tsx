6ed40804e2c4e7c14af6e3c693a457e0c4a57f64
moisesph <mois.mp8@gmail.com>
2023-06-17 23:14:36 -0500

    si

--- Nodens-Frontend/src/components/IndexLink/IndexLink.tsx
+++ Nodens-Frontend/src/components/IndexLink/IndexLink.tsx
@@ -5,7 +5,7 @@ import { Logo } from '../Logo';
 function IndexLink() {
   return (
     <Link to="/" className="cursor-pointer flex items-center justify-center">
-      <Logo dimensions='h-8 w-8'/>
+      <Logo dimensions='h-9 w-9'/>
     </Link>
   );
 }
--- Nodens-Frontend/src/components/NavMusician/NavMusicianRes.tsx
+++ Nodens-Frontend/src/components/NavMusician/NavMusicianRes.tsx
@@ -12,42 +12,72 @@ const NavMusicianRes = ({perfil} : {perfil : string}) => {
 		location.reload()
   }
   return (
-    <nav className="hidden md:flex fixed bg-slate-900 items-center justify-around h-[7vh] w-full z-50">
-      <div className="w-full flex justify-center items-center lg:w-2/3">
-        <Link to='/' className="flex"><IndexLink /><h3 className="text-slate-50 font-semibold text-xl">NODENS</h3></Link>
-        <div className="w-3/4 flex justify-between lg:justify-end gap-[10%] lg:gap-[4%]">
-          <Link className="h-[10%] flex flex-col items-center  rounded-lg text-slate-100 text-base after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2" 
-            to="/">
-              <FaHome className="h-8 w-8" />
-              <span className="text-sm">Inicio</span>
-            </Link>
-          <Link className="h-[10%] flex flex-col items-center  rounded-lg text-slate-100 text-base after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2" 
-            to="/offers">
-              <FaSuitcase className="h-8 w-8" />
-              <span className="text-sm">Oportunidades</span>
-            </Link>
-          <Link className="h-[10%] flex flex-col items-center  rounded-lg text-slate-100 text-base after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2" 
-            to="/applicants-offers">
-              <MdBookmark className="h-8 w-8" />
-              <span className="text-sm">Ofertas aplicadas</span>
-            </Link>
-          <Link className="h-[10%] flex flex-col items-center  rounded-lg text-slate-100 text-base after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2" 
-            to="/mainprofile">
-              {perfil.length > 0 ? 
-                <img src={perfil} className="h-8 w-8 rounded-full object-cover" />
-                :
-                <FaUserCircle className="h-8 w-8" />
-              }
-              <span className="text-sm">Perfil</span>
-            </Link>
-          <button onClick={()=>logOut()} className="h-fit flex flex-col items-center rounded-lg text-slate-100 text-base after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2" >
-            <AiOutlineLogout className="h-8 w-8" />
+    <nav className="hidden md:flex fixed bg-slate-900 items-center justify-around h-fit w-full z-50">
+      <div className="w-full flex justify-center items-center lg:w-2/3 h-fit">
+        <Link to="/" className="flex items-center">
+          <IndexLink />
+          <h3 className="text-slate-50 font-semibold text-xl">NODENS</h3>
+        </Link>
+        <div className="w-3/4 flex justify-between items-center lg:justify-end gap-[10%] lg:gap-[4%]">
+          <Link
+            className={`h-[10%] flex flex-col items-center  rounded-lg ${
+              location.pathname === "/"
+                ? "text-orange-600 after:bg-orange-600"
+                : "text-slate-100 after:bg-white"
+            } text-base after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100  py-[5px]`}
+            to="/"
+          >
+            <FaHome className="h-6 w-6" />
+            <span className="text-sm">Inicio</span>
+          </Link>
+          <Link
+            className={`h-[10%] flex flex-col items-center  rounded-lg ${
+              location.pathname === "/offers"
+                ? "text-orange-600 after:bg-orange-600"
+                : "text-slate-100 after:bg-white"
+            } text-base after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100  py-[5px]`}
+            to="/offers"
+          >
+            <FaSuitcase className="h-6 w-6" />
+            <span className="text-sm">Oportunidades</span>
+          </Link>
+          <Link
+            className={`h-[10%] flex flex-col items-center  rounded-lg ${
+              location.pathname === "/applicants-offers"
+                ? "text-orange-600 after:bg-orange-600"
+                : "text-slate-100 after:bg-white"
+            } text-base after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100  py-[5px]`}
+            to="/applicants-offers"
+          >
+            <MdBookmark className="h-6 w-6" />
+            <span className="text-sm">Ofertas aplicadas</span>
+          </Link>
+          <Link
+            className={`h-[10%] flex flex-col items-center  rounded-lg ${
+              location.pathname === "/mainprofile"
+                ? "text-orange-600 after:bg-orange-600"
+                : "text-slate-100 after:bg-white"
+            } text-base after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100  py-[5px]`}
+            to="/mainprofile"
+          >
+            {perfil.length > 0 ? (
+              <img src={perfil} className="h-6 w-6 rounded-full object-cover" />
+            ) : (
+              <FaUserCircle className="h-6 w-6" />
+            )}
+            <span className="text-sm">Perfil</span>
+          </Link>
+          <button
+            onClick={() => logOut()}
+            className="h-fit flex flex-col items-center rounded-lg text-slate-100 text-base after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-[5px]"
+          >
+            <AiOutlineLogout className="h-6 w-6" />
             <span className="text-sm">Cerrar Sesion</span>
           </button>
         </div>
       </div>
     </nav>
-  )
+  );
 }
 
 export default NavMusicianRes
