eea6cee1f4f10678bd9f418686bad3cd54a0f392
Sebastián García Correa <sebastian435622@gmail.com>
2023-06-20 13:55:34 -0500

    Mejor estilo de posts pa

--- src/pages/Posts/Posts.tsx
+++ src/pages/Posts/Posts.tsx
@@ -6,11 +6,11 @@ import axios, { Axios } from "axios"
 import { clientHttp } from "../../services/client";
 import { Loading } from "../../components";
 import { FaUser } from "react-icons/fa"
- 
 
 
 
-const Posts = ({profImg} : {profImg : string}) => {
+
+const Posts = ({ profImg }: { profImg: string }) => {
 
   const PosTs = ([
     {
@@ -86,53 +86,82 @@ const Posts = ({profImg} : {profImg : string}) => {
     <>
       <main className="bg-[#F3F2EF] absolute w-full h-fit">
 
-        {
-          //Crear Publicaciones
-        }
-        <div className="pt-10 flex justify-center items-center ">
-          <div className="bg-[#FFFFFF] h-[12vh] w-[30vw] pt-2 text-center flex justify-center items-start rounded-2xl gap-2"> 
+
+
+        <div className="pt-10 flex justify-center items-start gap-10">
           {
-                          profImg.length > 0 ?
-                            <img src={profImg} alt="" className="h-11 w-12 rounded-xl"/>
-                            :
-                            <FaUser className="h-10 w-10"/>
-                        } 
+            //Mensaje de bienvenida
+          }
+
+            <div className="bg-[#FFFFFF] w-[12vw] h-[30vh] rounded-2xl ">
+              <p>Bienvenido pa</p>
+            </div>
+
+          <div className="bg-[#FFFFFF] h-[12vh] w-[30vw] pt-2 text-center flex justify-center items-start rounded-2xl gap-2">
+            {
+              //Creaciòn de publicaciones
+            }
+            {
+              profImg.length > 0 ?
+                <img src={profImg} alt="" className="h-11 w-12 rounded-xl" />
+                :
+                <FaUser className="h-10 w-10" />
+            }
             <button className="bg-transparent h-[5vh] w-[25vw] border border-zinc-950 rounded-3xl transition-all ease-in-out hover:bg-[#F3F2EF]  hover:duration-200">Crear Publicación</button>
           </div>
+
+
+
+          {
+            //Páginas que puedes seguir y el footer
+          }
+            <div className="bg-[#FFFFFF] w-[12vw] h-[30vh] rounded-2xl">
+              El footer y Páginas interesantes pa
+            </div>
         </div>
-        <section className="w-full ">
+
+        {
+          //Recomendaciones
+        }
+        <div className="flex justify-center translate-y-[-16vh]">
+          <div className="bg-[#FFFFFF] w-[30vw] h-[15vh] rounded-2xl">
+            <p>Recomendaciones pa</p>
+          </div>
+        </div>
+
+        <section className="w-full h-fit z-30">
           {
             // Map
           }
-          <div className="overflow-y-scroll h-[95vh] pt-7 ">
+          <div className="">
 
             {
               PosTs.map((k, index) => (
                 <>
                   <section key={index}>
-                    <div className="pb-10 pt-2 flex justify-center items-center ">
+                    <div className="pb-10 pt-2 flex justify-center items-center translate-y-[-14vh] ">
 
                       <div className="text-black bg-[#FFFFFF] w-[30vw] min-h-[60vh] flex flex-col justify-center items-center rounded-xl">
                         <div>
-                        {
-                          profImg.length > 0 ?
-                            <img src={profImg} alt="" className="h-8 w-8 rounded-xl"/>
-                            :
-                            <FaUser className="h-8 w-8"/>
-                        } 
-                        <h1>
-                          {k.id}
-                        </h1>
-                        <p>
-                          {k.name1}
-                        </p>
-                        <p>
-                          {k.number}
-                        </p>
-                          <img src={k.img} alt="" className="w-[25vw] h-[35vh]"/>
-                        <p>
-                          {k.texto}
-                        </p>
+                          {
+                            profImg.length > 0 ?
+                              <img src={profImg} alt="" className="h-8 w-8 rounded-xl" />
+                              :
+                              <FaUser className="h-8 w-8" />
+                          }
+                          <h1>
+                            {k.id}
+                          </h1>
+                          <p>
+                            {k.name1}
+                          </p>
+                          <p>
+                            {k.number}
+                          </p>
+                          <img src={k.img} alt="" className="w-[25vw] h-[35vh]" />
+                          <p>
+                            {k.texto}
+                          </p>
                         </div>
                       </div>
                     </div>
