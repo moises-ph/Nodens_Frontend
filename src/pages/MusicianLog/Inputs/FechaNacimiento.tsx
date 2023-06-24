cff820440bfa63345b00f9390b33bf8cdc1fd2b5
Camilo Forero <mois.mp8@gmail.com>
2023-06-24 11:08:53 -0500

    Pa que haga los organizadores tambien

--- src/pages/MusicianLog/Inputs/FechaNacimiento.tsx
+++ src/pages/MusicianLog/Inputs/FechaNacimiento.tsx
@@ -28,7 +28,7 @@ const FechaNacimiento = ({ handler, actualMusician }: { handler: (key: string, v
 	return (
 		<div className='bg-white transition-all bg-opacity-100 h-full rounded-lg w-10/12 flex flex-col justify-center gap-8 py-10 px-24 text-black/90 shadow-xl'>
 			<label htmlFor='fecha_nacimiento' className='text-2xl h-2/4 flex flex-col gap-[20%]'>Fecha de nacimiento:
-				<input type="date" onChange={() => setIsValue(true)} name="fecha_nacimiento" ref={fecha_nacimiento} className='bg-transparent md:w-1/3 border-solid border-0 border-b-2 border-slate-400 text-black/70 font-thin'/>
+				<input type="date" onChange={() => setIsValue(fecha_nacimiento.current!.value ? true : false)} name="fecha_nacimiento" ref={fecha_nacimiento} className='bg-transparent md:w-1/3 border-solid border-0 border-b-2 border-slate-400 text-black/70 font-thin'/>
 			</label>
 			<div className='flex flex-row gap-4'>
 				<button className='px-4 bg-blue-700 disabled:bg-blue-400 rounded-md text-slate-100 h-8' disabled={!isValue} onClick={() => checking()}>Guardar</button>
--- src/pages/MusicianLog/Inputs/Name.tsx
+++ src/pages/MusicianLog/Inputs/Name.tsx
@@ -1,8 +1,8 @@
-import { useRef, useState } from "react"
+import { useRef, useState, useEffect } from 'react';
 import Swal from "sweetalert2"
 
 
-const Name = ({ handler, goBack, setNombre }: { handler: (key: string, value: any) => void, goBack: ()=>void, setNombre : (key : string, value : any) => void }) => {
+const Name = ({ handler, goBack, setNombre, actualMusician }: { handler: (key: string, value: any) => void, goBack: ()=>void, setNombre : (key : string, value : any) => void, actualMusician : any }) => {
   const name = useRef<HTMLInputElement>(null)
   const Lastname = useRef<HTMLInputElement>(null);
   const [isValue, setIsValue] = useState<boolean>(false);
@@ -12,7 +12,7 @@ const Name = ({ handler, goBack, setNombre }: { handler: (key: string, value: an
       Swal.fire({
         icon: 'error',
         title: 'Oops...',
-        text: 'Por favor ingresa un nombre valido',
+        text: 'Por favor ingresa un nombre o apellido valido',
         timer: 2000  
       })
     } else {
@@ -20,16 +20,21 @@ const Name = ({ handler, goBack, setNombre }: { handler: (key: string, value: an
     }
   }
 
+  useEffect(() => {
+    if(actualMusician.Name.length > 0 && actualMusician.Lastname.length > 0){
+      name.current!.value = actualMusician.Name;
+      Lastname.current!.value = actualMusician.Lastname;
+      setIsValue(true);
+    }
+  })
+
   return (
   <>
     <div className='bg-white transition-all bg-opacity-100 h-full rounded-lg w-10/12 flex flex-col justify-center gap-8 py-10 px-24 text-black/90 shadow-xl'>
-      <div className="flex flex-col gap-10 h-fit">
-        <label htmlFor='genero' className='flex flex-col text-2xl h-2/4 gap-3'>Cual es tu nombre?
-          <input type="text" name="telefono" ref={name} onChange={() => setIsValue(true)} className='w-1/3 bg-transparent border-solid border-b-2 border-slate-300 text-black/70 font-thin outline-none focus:border-slate-500'/>
-        </label>
-        <label htmlFor='genero' className='flex flex-col text-2xl h-2/4 gap-3'>Apellido:
-          <input type="text" name="telefono" ref={Lastname} className='bg-transparent border-solid border-b-2 w-1/3 border-slate-300 text-black/70 font-thin outline-none focus:border-slate-500'/>
-        </label>
+      <label htmlFor='genero' className='flex flex-col text-2xl gap-3'>Cual es tu nombre?</label>
+      <div className="flex gap-10 h-fit">
+        <input type="text" name="telefono" placeholder='Nombre' ref={name} onChange={() => setIsValue(name.current!.value.length > 3 && Lastname.current!.value.length > 3)} className='w-1/4| bg-transparent border-solid border-b-2 border-slate-300 text-black/70 text-xl placeholder:text-base placeholder:italic font-thin outline-none focus:border-slate-500'/>
+        <input type="text" name="telefono" placeholder='Apelllido' ref={Lastname} onChange={() => setIsValue(name.current!.value.length > 3 && Lastname.current!.value.length > 3)} className='bg-transparent border-solid border-b-2 w-1/4 border-slate-300 text-black/70 text-xl placeholder:text-base placeholder:italic font-thin outline-none focus:border-slate-500'/>
       </div>
       <div className="flex gap-4">
         <button onClick={() => goBack()} className='px-4 bg-orange-500 rounded-md text-slate-100 h-8'>Atras</button>
