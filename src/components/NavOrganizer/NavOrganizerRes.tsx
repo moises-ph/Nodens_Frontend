064013109905828fb7a05f291f8da09b85266500
Sebastián García Correa <sebastian435622@gmail.com>
2023-06-27 13:52:34 -0500

    Melo

--- src/components/NavOrganizer/NavOrganizerRes.tsx
+++ src/components/NavOrganizer/NavOrganizerRes.tsx
@@ -4,6 +4,7 @@ import { FaUser } from "react-icons/fa"
 import { BsFillPlusCircleFill, BsFillBookmarkFill } from "react-icons/bs"
 import { HiUsers } from "react-icons/hi"
 import { AiOutlineLogout } from "react-icons/ai"
+import { BsFileEarmarkPost } from "react-icons/bs"
  
 
 
@@ -15,25 +16,46 @@ const NavOrganizerRes = ({profImg} : {profImg : string}) => {
 		location.reload()
 	}
   return (
-    <nav className="hidden md:flex fixed bg-slate-900 items-center justify-start h-[7vh] lg:gap-4 gap-[1%] w-full z-50">
-      <Link to="/" className="flex"><IndexLink /><h3 className="text-slate-50 font-semibold">NODENS</h3></Link>
-      <div className="w-full flex justify-between items-center lg:justify-end gap-[7%] pr-3">
-        <Link className="h-[10%] flex flex-col items-center  rounded-lg text-slate-100 text-xs after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2" 
-          to="/posts"> Posts</Link>
-        <Link className="h-[10%] flex flex-col items-center gap-1 rounded-lg text-slate-100 text-xs after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2" 
-          to="/profiles"><HiUsers className="h-8 w-8"/> Perfiles</Link>
-        <Link className="h-[10%] flex flex-col items-center gap-1 rounded-lg text-slate-100 text-xs after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2" 
-          to="/create-offer"><BsFillPlusCircleFill className="h-8 w-8"/> Crear Oferta</Link>
-        <Link className="h-[10%] flex flex-col items-center gap-1 rounded-lg text-slate-100 text-xs after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2" 
-          to="/offers"><BsFillBookmarkFill className="h-8 w-8"/>Tus Ofertas</Link>
-         <Link className="h-[10%] flex flex-col items-center gap-1 rounded-lg text-slate-100 text-xs after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2" 
+    <nav className="hidden md:flex fixed bg-slate-900 items-center justify-around h-[7vh] lg:gap-4 gap-[1%] w-full z-50">
+      <div className="flex items-center justify-evenly w-full">
+      <Link to="/" className="flex items-center translate-x-[25rem]"><IndexLink /><h3 className="text-slate-50 font-semibold">NODENS</h3></Link>
+      <div className="w-full flex justify-evenly items-center lg:justify-end gap-[2%] pr-20 translate-x-[-20rem]">
+        <Link className={`h-[10%] flex flex-col items-center  rounded-lg text-slate-100 text-xs after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2 ${
+          location.pathname === "/posts"
+          ? "text-orange-600 after:bg-orange-600"
+          : "text-slate-100 after:bg-white"
+        }`} 
+          to="/posts"><BsFileEarmarkPost className="h-6 w-6 "/> Posts</Link>
+        <Link className={`h-[10%] flex flex-col items-center gap-1 rounded-lg text-slate-100 text-xs after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2 ${
+          location.pathname === "/profiles"
+          ? "text-orange-600 after:bg-orange-600"
+          : "text-slate-100 after:bg-white"
+        }`} 
+          to="/profiles"><HiUsers className="h-6 w-6"/> Perfiles</Link>
+        <Link className={`h-[10%] flex flex-col items-center gap-1 rounded-lg text-slate-100 text-xs after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2 ${
+          location.pathname === "/create-offer"
+          ? "text-orange-600 after:bg-orange-600"
+          : "text-slate-100 after:bg-white"
+        }`} 
+          to="/create-offer"><BsFillPlusCircleFill className="h-6 w-6"/> Crear Oferta</Link>
+        <Link className={`h-[10%] flex flex-col items-center gap-1 rounded-lg text-slate-100 text-xs after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2 ${
+          location.pathname === "/offers"
+          ? "text-orange-600 after:bg-orange-600"
+          : "text-slate-100 after:bg-white"
+        }`} 
+          to="/offers"><BsFillBookmarkFill className="h-6 w-6"/>Tus Ofertas</Link>
+         <Link className={`h-[10%] flex flex-col items-center gap-1 rounded-lg text-slate-100 text-xs after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2 ${
+          location.pathname === "/mainprofile"
+          ? "text-orange-600 after:bg-orange-600"
+          : "text-slate-100 after:bg-white"
+         }`} 
           to="/mainprofile"> 
           {
             profImg.length > 0 ?
 
-            <img src={profImg} alt="" className="h-8 w-8 rounded-full object-cover" />
+            <img src={profImg} alt="" className="h-6 w-6 rounded-full object-cover" />
               :
-              <FaUser className="h-8 w-8"/>
+              <FaUser className="h-6 w-6"/>
           }
           <span> Perfil </span>
           </Link>
@@ -43,6 +65,7 @@ const NavOrganizerRes = ({profImg} : {profImg : string}) => {
 
           
         <button onClick={close} className="h-[10%] flex flex-col items-center rounded-lg text-slate-100 text-xs after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2" ><AiOutlineLogout className="h-8 w-8"/> Cerrar Sesion</button>
+        </div>
       </div>
     </nav>
   )
