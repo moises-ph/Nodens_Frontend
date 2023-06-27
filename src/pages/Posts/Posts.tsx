b68e1608f7c51e29ff5f6671f20fa14e3eeb90e0
moisesph <mois.mp8@gmail.com>
2023-06-27 14:07:59 -0500

    Posts: seguir!

--- src/pages/Posts/Posts.tsx
+++ src/pages/Posts/Posts.tsx
@@ -88,83 +88,63 @@ const Posts = ({ profImg, nameProf }: { profImg: string, nameProf: string }) => 
   }
   return (
     <>
-      <main className="bg-[#F3F2EF] absolute w-full h-fit">
-
-
-
-        <div className="pt-10 flex justify-center items-start gap-10">
-          {
-            //Mensaje de bienvenida
-          }
-
-          <div className="bg-[#FFFFFF] w-[12vw] h-fit rounded-2xl pl-2 pr-2 pt-2 justify-start flex flex-col">
-            {
-              profImg.length > 0 ?
-                <img src={profImg} alt="" className="h-[4rem] w-fit rounded-full" />
-                :
-                <FaUser className="h-8 w-8" />
-            }
-            <p className="text-sm">Hola {
-              nameProf.length > 0 ?
-                <p className="text-sm">{nameProf}</p>
-                :
-                <p>User</p>
-            }</p>
-            <p className="text-sm">Bienvenido a NODENS</p>
-          </div>
-
-          <div className="bg-[#FFFFFF] h-[12vh] w-[30vw] pt-2 text-center flex justify-center items-start rounded-2xl gap-2">
-            {
-              //Creaciòn de publicaciones
-            }
-            {
-              profImg.length > 0 ?
-                <img src={profImg} alt="" className="h-[5rem] w-fit rounded-full pl-1" />
-                :
-                <FaUser className="h-10 w-10" />
-            }
-            <button className="bg-transparent h-[5vh] w-[25vw] border border-zinc-950 rounded-3xl transition-all ease-in-out hover:bg-[#F3F2EF]  hover:duration-200">Crear Publicación</button>
-          </div>
-
-
-
-          {
-            //Páginas que puedes seguir y el footer
-          }
-          <div className="bg-[#FFFFFF] w-[12vw] h-[30vh] rounded-2xl">
-            El footer y Páginas interesantes pa
-          </div>
+      <main className="bg-[#F3F2EF] py-10 flex justify-center items-start absolute w-full h-fit gap-8 overflow-y-hidden">
+        <div className="flex flex-col bg-white justify-center border-solid border-black/20 border-[1px] rounded-2xl ">
+          {profImg.length > 0 ? (
+            <img
+              src={profImg}
+              alt=""
+              className="h-[8rem] object-contain rounded-full"
+            />
+          ) : (
+            <FaUser className="h-8 w-8" />
+          )}
+          <p className="text-sm">
+            Bienvenido <span className="text-sm">{nameProf}</span>
+          </p>
         </div>
 
-        <section className="w-full h-fit z-30">
-          {
-            // Map
-          }
-          <div className="">
-
-            {
-              PosTs.map((k, index) => (
-                <>
-                  <section key={index}>
-                    <div className="pb-10 pt-2 flex justify-center items-center translate-y-[-14vh] ">
-
-                      <div className="text-black bg-[#FFFFFF] w-[30vw] min-h-[60vh] pl-2 pr-2 pb-4 pt-4 flex flex-col items-center rounded-xl shadow-2xl">
-                        <div>
-                          <p className="pl-1 underline">
-                            {k.name1}
-                          </p>
-                          <p>{renderedText}</p>
-                          <button className="text-blue-500 underline" onClick={toggleText}>{showFull ? "Acortar" : "Mostrar Completo"}</button>
-                          <img src={k.img} alt="" className="w-fit h-fit" />
-                        </div>
+        <section className="w-1/3 h-fit max-h-[84vh] flex flex-col items-center overflow-y-auto">
+          <div className="bg-white h-fit w-full px-3 py-2 text-center flex flex-col rounded-2xl gap-2">
+            <div className="flex items-center justify-between gap-1">
+              {profImg.length > 0 ? (
+                <img
+                  src={profImg}
+                  className="rounded-full border-solid border-black/40 border-[1px] h-[4rem] object-contain"
+                />
+              ) : (
+                <FaUser></FaUser>
+              )}
+              <input className="w-full border-solid border-[1px] border-black/40 rounded-2xl h-[2rem]" />
+            </div>
+          </div>
+          <div className="w-full before:w-full before:h-[1px] before:bg-black">
+            {PosTs.map((k, index) => (
+              <>
+                <div key={index}>
+                  <div className="pt-2 flex justify-center items-center">
+                    <div className="border-solid border-black/20 border-[1px] text-black bg-[#FFFFFF] w-full min-h-[60vh] pl-2 pr-2 pb-4 pt-4 flex flex-col items-center rounded-xl ">
+                      <div>
+                        <p className="pl-1 underline">{k.name1}</p>
+                        <p>{renderedText}</p>
+                        <button
+                          className="text-blue-500 underline"
+                          onClick={toggleText}
+                        >
+                          {showFull ? "Acortar" : "Mostrar Completo"}
+                        </button>
+                        <img src={k.img} alt="" className="w-fit h-fit" />
                       </div>
                     </div>
-                  </section>
-                </>
-              ))
-            }
+                  </div>
+                </div>
+              </>
+            ))}
           </div>
         </section>
+        <footer className="bg-[#FFFFFF] w-[12vw] h-[30vh] rounded-2xl">
+          El footer y Páginas interesantes pa
+        </footer>
       </main>
     </>
   );
