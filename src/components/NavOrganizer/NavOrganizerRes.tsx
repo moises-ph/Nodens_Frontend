6504c6609afd228e68e16c58a38330277b1d0f75
Sebastián García Correa <sebastian435622@gmail.com>
2023-06-28 10:36:27 -0500

    Melo

--- src/components/NavOrganizer/NavOrganizerRes.tsx
+++ src/components/NavOrganizer/NavOrganizerRes.tsx
@@ -18,33 +18,33 @@ const NavOrganizerRes = ({profImg} : {profImg : string}) => {
   return (
     <nav className="hidden md:flex fixed bg-slate-900 items-center justify-around h-[7vh] lg:gap-4 gap-[1%] w-full z-50">
       <div className="w-full flex justify-center items-center lg:w-2/3 h-fit">
-      <Link to="/" className="flex items-center translate-x-[25rem] md:translate-x-5"><IndexLink /><h3 className="text-slate-50 font-semibold">NODENS</h3></Link>
+      <Link to="/" className="flex items-center translate-x-[25rem] md:translate-x-1"><IndexLink /><h3 className="text-slate-50 font-semibold">NODENS</h3></Link>
       <div className="w-3/4 flex justify-between items-center lg:justify-end gap-[10%] lg:gap-[4%]">
-        <Link className={`h-[10%] flex flex-col items-center  rounded-lg text-slate-100 text-xs after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2 ${
+        <Link className={`h-[10%] flex flex-col items-center  rounded-lg text-slate-100 text-xs text-center after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2 ${
           location.pathname === "/posts"
           ? "text-orange-600 after:bg-orange-600"
           : "text-slate-100 after:bg-white"
         }`} 
           to="/posts"><BsFileEarmarkPost className="h-6 w-6 "/> Posts</Link>
-        <Link className={`h-[10%] flex flex-col items-center gap-1 rounded-lg text-slate-100 text-xs after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2 ${
+        <Link className={`h-[10%] flex flex-col items-center gap-1 rounded-lg text-slate-100 text-xs text-center after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2 ${
           location.pathname === "/profiles"
           ? "text-orange-600 after:bg-orange-600"
           : "text-slate-100 after:bg-white"
         }`} 
           to="/profiles"><HiUsers className="h-6 w-6"/> Perfiles</Link>
-        <Link className={`h-[10%] flex flex-col items-center gap-1 rounded-lg text-slate-100 text-xs after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2 ${
+        <Link className={`h-[10%] flex flex-col items-center gap-1 rounded-lg text-slate-100 text-xs text-center after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2 ${
           location.pathname === "/create-offer"
           ? "text-orange-600 after:bg-orange-600"
           : "text-slate-100 after:bg-white"
         }`} 
           to="/create-offer"><BsFillPlusCircleFill className="h-6 w-6"/> Crear Oferta</Link>
-        <Link className={`h-[10%] flex flex-col items-center gap-1 rounded-lg text-slate-100 text-xs after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2 ${
+        <Link className={`h-[10%] flex flex-col items-center gap-1 rounded-lg text-slate-100 text-xs text-center after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2 ${
           location.pathname === "/offers"
           ? "text-orange-600 after:bg-orange-600"
           : "text-slate-100 after:bg-white"
         }`} 
           to="/offers"><BsFillBookmarkFill className="h-6 w-6"/>Tus Ofertas</Link>
-         <Link className={`h-[10%] flex flex-col items-center gap-1 rounded-lg text-slate-100 text-xs after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2 ${
+         <Link className={`h-[10%] flex flex-col items-center gap-1 rounded-lg text-slate-100 text-xs text-center after:content-['_'] after:h-[1px] after:w-full after:scale-x-0 after:origin-left after:transition after:transform after:duration-150 after:ease-linear hover:after:scale-x-100 after:bg-white py-4 pl-2 ${
           location.pathname === "/mainprofile"
           ? "text-orange-600 after:bg-orange-600"
           : "text-slate-100 after:bg-white"
--- src/pages/CreateOffer/CreateOffer.tsx
+++ src/pages/CreateOffer/CreateOffer.tsx
@@ -127,47 +127,47 @@ const CreateOffer = () => {
   
   return (
     <>
-      <main className="w-full h-fit flex flex-col items-center justify-center gap-2 pt-6 bg-slate-200">
+      <main className="w-full h-fit flex flex-col items-center justify-center gap-2 pt-10 bg-slate-200">
         <h1 className="text-2xl font-semibold text-slate-800">Crear Oferta</h1>
         {loading && <div className={`absolute right-4 md:right-[37rem] top-[4.25rem] md:top-[101.7rem] flex items-center justify-center`}><div className='w-8 h-8 rounded-[50%] [border-left-style:solid] border-[11.2px] border-double border-[#474bff] animate-spin'></div></div>}
         <form className="w-full md:w-4/6 lg:w-3/6 flex flex-col items-center bg-transparent gap-4 pb-6 z-30" onSubmit={(e)=> e.preventDefault()}>
-          <label htmlFor="Title" className="w-5/6 h-1/6 bg-zinc-900 rounded-xl p-3 flex flex-col gap-6">
-            <h5 className="text-xl font-semibold text-slate-100">Titulo de Oferta <span className="text-red-700">*</span></h5>
+          <label htmlFor="Title" className="w-5/6 h-1/6 bg-slate-100 shadow-2xl border-solid border-[1px] border-black rounded-xl p-3 flex flex-col gap-6">
+            <h5 className="text-xl font-semibold text-gray-900">Titulo de Oferta <span className="text-red-700">*</span></h5>
             <input type="text" id="Title" name="Title" placeholder="Titulo"  ref={title} className="h-2/5 border-solid border-[1px] border-slate-900 rounded-md" required/>
           </label>
-          <label htmlFor="Description" className="w-5/6 h-[20vh] bg-zinc-900 rounded-xl p-3 flex flex-col gap-4">
-            <h5 className="text-xl font-semibold text-slate-100">Descripcion de Oferta <span className="text-red-700">*</span></h5>
+          <label htmlFor="Description" className="w-5/6 h-[20vh] bg-slate-100 shadow-2xl border-solid border-[1px] border-black rounded-xl p-3 flex flex-col gap-4">
+            <h5 className="text-xl font-semibold text-gray-900">Descripcion de Oferta <span className="text-red-700">*</span></h5>
             <textarea name="Description" id="" ref={description} className="h-4/5 resize-none border-solid border-[1px] border-slate-900 rounded-md" required></textarea>
           </label>
-          <label htmlFor="Event_Date" className="w-5/6 h-1/6 bg-zinc-900 rounded-xl p-3 flex flex-col gap-6">
-            <h5 className="text-xl font-semibold text-slate-100">Fecha del evento <span className="text-red-700">*</span></h5>
+          <label htmlFor="Event_Date" className="w-5/6 h-1/6 bg-slate-100 shadow-2xl border-solid border-[1px] border-black rounded-xl p-3 flex flex-col gap-6">
+            <h5 className="text-xl font-semibold text-gray-900">Fecha del evento <span className="text-red-700">*</span></h5>
             <input min={new Date().toISOString().slice(0,16)} type="datetime-local" name="Event_Date" ref={event_date} className="h-2/5 border-solid border-[1px] border-slate-900 rounded-md" required/>
           </label>
-          <label htmlFor="Payment" className="w-5/6 h-1/6 bg-zinc-900 rounded-xl p-3 flex flex-col gap-6">
-            <h5 className="text-xl font-semibold text-slate-100">Pago de la oferta <span className="text-red-700">*</span></h5>
+          <label htmlFor="Payment" className="w-5/6 h-1/6 bg-slate-100 shadow-2xl border-solid border-[1px] border-black rounded-xl p-3 flex flex-col gap-6">
+            <h5 className="text-xl font-semibold text-gray-900">Pago de la oferta <span className="text-red-700">*</span></h5>
             <input type="number" name="Payment" placeholder="Pago" ref={payment} className="h-2/5 border-solid border-[1px] border-slate-900 rounded-md" required/>
           </label>
           
-          <label className="w-5/6 h-full bg-zinc-900 rounded-xl p-3 flex flex-col gap-4">
-            <h5 className="text-xl font-semibold text-slate-100">Ubicacion del evento <span className="text-red-700">*</span></h5>
-            <label htmlFor="Career" className="text-slate-100">Carrera:</label>
+          <label className="w-5/6 h-full bg-slate-100 shadow-2xl border-solid border-[1px] border-black rounded-xl p-3 flex flex-col gap-4">
+            <h5 className="text-xl font-semibold text-gray-900">Ubicacion del evento <span className="text-red-700">*</span></h5>
+            <label htmlFor="Career" className="text-gray-900">Carrera:</label>
             <input type="text" name="Career" placeholder="Carrera" ref={career} className="h-2/5 border-solid border-[1px] border-slate-900 rounded-md" />
-            <label htmlFor="Street" className="text-slate-100">Calle:</label>
+            <label htmlFor="Street" className="text-gray-900">Calle:</label>
             <input type="text" name="Street" placeholder="Calle" ref={street} className="h-2/5 border-solid border-[1px] border-slate-900 rounded-md" />
-            <label htmlFor="City" className="text-slate-100">Ciudad: <span className="text-red-700">*</span></label>
+            <label htmlFor="City" className="text-gray-900">Ciudad: <span className="text-red-700">*</span></label>
             <input type="text" name="City" placeholder="Ciudad" ref={city} className="h-2/5 border-solid border-[1px] border-slate-900 rounded-md" required/>
-            <label htmlFor="SiteNumber" className="text-slate-100">Numero: <span className="text-red-700">*</span></label>
+            <label htmlFor="SiteNumber" className="text-gray-900">Numero: <span className="text-red-700">*</span></label>
             <input type="text" name="SiteNumber" placeholder="Numero" ref={site_number} className="h-2/5 border-solid border-[1px] border-slate-900 rounded-md" required/>
-            <label htmlFor="Town" className="text-slate-100">Barrio: <span className="text-red-700">*</span></label>
+            <label htmlFor="Town" className="text-gray-900">Barrio: <span className="text-red-700">*</span></label>
             <input type="text" name="Town" placeholder="Barrio" ref={town} className="h-2/5 border-solid border-[1px] border-slate-900 rounded-md" required/>
           </label>
 
-          <label htmlFor="Vacants" className="w-5/6 h-1/6 bg-zinc-900 rounded-xl p-3 flex flex-col gap-6">
-            <h5 className="text-xl font-semibold text-slate-100">Vacantes: <span className="text-red-700">*</span></h5>
+          <label htmlFor="Vacants" className="w-5/6 h-1/6 bg-slate-100 shadow-2xl border-solid border-[1px] border-black rounded-xl p-3 flex flex-col gap-6">
+            <h5 className="text-xl font-semibold text-gray-900">Vacantes: <span className="text-red-700">*</span></h5>
             <input type="number" name="Vacants" placeholder="Vacantes" ref={vacants} className="h-2/5 border-solid border-[1px] border-slate-900 rounded-md" required/>
           </label>
-          <label className="w-5/6 h-1/6 bg-zinc-900 rounded-xl p-3 flex flex-col gap-6">
-            <h5 className="text-xl font-semibold text-slate-100">Requerimientos <span className="text-red-700">*</span></h5>
+          <label className="w-5/6 h-1/6 bg-slate-100 shadow-2xl border-solid border-[1px] border-black rounded-xl p-3 flex flex-col gap-6">
+            <h5 className="text-xl font-semibold text-gray-900">Requerimientos <span className="text-red-700">*</span></h5>
             {
               requeriments.map((req, i ) => {
                 return <div key={i} className='flex items-center justify-between px-3 text-base  bg-slate-100 h-4/5 col-span-1 rounded-md text-slate-700 font-normal border-solid border-slate-600 border-[1px]'>
@@ -177,10 +177,10 @@ const CreateOffer = () => {
               })
             }
             <input type="text" name="requeriment" placeholder="Requerimiento" ref={requeriment} className="h-2/5 border-solid border-[1px] border-slate-900 rounded-md"/>
-            <span onClick={(e)=>{e.preventDefault(); addRequeriment();}} className="text-slate-100">Agregar Requerimiento</span>
+            <span onClick={(e)=>{e.preventDefault(); addRequeriment();}} className="text-gray-900">Agregar Requerimiento</span>
           </label>
-          <label className="w-5/6 h-1/6 bg-zinc-900 rounded-xl p-3 flex flex-col gap-6">
-            <h5 className="text-xl font-semibold text-slate-100">Tags <span className="text-red-700">*</span></h5>
+          <label className="w-5/6 h-1/6 bg-slate-100 shadow-2xl border-solid border-[1px] border-black rounded-xl p-3 flex flex-col gap-6">
+            <h5 className="text-xl font-semibold text-gray-900">Tags <span className="text-red-700">*</span></h5>
             {
               tags.map((req, i ) => {
                 return <div key={i} className='flex items-center justify-between px-3 text-base  bg-slate-100 h-4/5 col-span-1 rounded-md text-slate-700 font-normal border-solid border-slate-600 border-[1px]'>
@@ -190,9 +190,9 @@ const CreateOffer = () => {
               })
             }
             <input type="text" name="tags" placeholder="tags" ref={tag} className="h-2/5 border-solid border-[1px] border-slate-900 rounded-md"/>
-            <span onClick={(e)=>{e.preventDefault() ; addTag()}} className="text-slate-100">Agregar Tag</span>
+            <span onClick={(e)=>{e.preventDefault() ; addTag()}} className="text-gray-900">Agregar Tag</span>
           </label>
-          <input type="submit" onClick={(e)=> handle(e)} className="w-2/6 h-10 bg-orange-500 text-slate-200 font-medium text-xl rounded-xl" value="Publicar"/>
+          <input type="submit" onClick={(e)=> handle(e)} className="w-2/6 h-10 bg-orange-500 text-slate-200 font-medium text-xl rounded-xl hover:shadow-2xl hover:shadow-black hover:ease-in hover:border-black hover:border hover:cursor-pointer hover:duration-200  active:translate-y-2 " value="Publicar"/>
         </form>
       </main>
     </>
